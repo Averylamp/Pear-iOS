9d970429116216365e51fa74c900619f092ea695
Avery Lamp <averylamp@gmail.com>
2019-02-08 15:26:55 -0500

    Added 4 landing pages

--- Pear/Controllers/Onboarding/LandingScreenViewController.swift
+++ Pear/Controllers/Onboarding/LandingScreenViewController.swift
@@ -13,6 +13,7 @@ class LandingScreenViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     
     /// Factory method for creating this view controller.
@@ -23,6 +24,7 @@ class LandingScreenViewController: UIViewController {
         let vc = storyboard.instantiateInitialViewController() as! LandingScreenViewController
         return vc
     }
+
     
     
 }
@@ -35,27 +37,55 @@ extension LandingScreenViewController{
         super.viewDidLoad()
         
         styleButtons()
-    }
-    
-    func styleButtons(){
-        signupButton.layer.shadowRadius = 3
-        signupButton.layer.shadowOffset = CGSize(width: 1, height: 1)
-        signupButton.layer.shadowColor = UIColor.black.cgColor
-        signupButton.layer.shadowOpacity = 0.4
-        
-        loginButton.layer.shadowRadius = 3
-        loginButton.layer.shadowOffset = CGSize(width: 1, height: 1)
-        loginButton.layer.shadowColor = UIColor.black.cgColor
-        loginButton.layer.shadowOpacity = 0.4
+        attachButtons()
+        setupScrollView()
         
     }
 
 }
 
+// MARK: - Styling
+extension LandingScreenViewController{
+    func styleButtons(){
+        signupButton.layer.shadowRadius = 3
+        signupButton.layer.shadowOffset = CGSize(width: 1, height: 1)
+        signupButton.layer.shadowColor = UIColor.black.cgColor
+        signupButton.layer.shadowOpacity = 0.4
+        signupButton.layer.cornerRadius = 4
+        
+        loginButton.layer.shadowRadius = 3
+        loginButton.layer.shadowOffset = CGSize(width: 1, height: 1)
+        loginButton.layer.shadowColor = UIColor.black.cgColor
+        loginButton.layer.shadowOpacity = 0.4
+        loginButton.layer.cornerRadius = 4
+    }
+    
+    func setupScrollView(){
+        let numPages: Int  = 4
+        scrollView.contentSize = CGSize(width: self.view!.frame.width * CGFloat(numPages), height: scrollView.frame.size.height)
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        pageControl.numberOfPages = numPages
+    }
+    
+    
+}
+
 
 // MARK: - @IBActions
 private extension LandingScreenViewController{
+    func attachButtons(){
+        signupButton.addTarget(self, action: #selector(LandingScreenViewController.signupButtonClicked(sender:)), for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(LandingScreenViewController.loginButtonClicked(sender:)), for: .touchUpInside)
+    }
     
+    @objc func signupButtonClicked(sender:UIButton){
+        print("Signup Clicked")
+    }
+    
+    @objc func loginButtonClicked(sender:UIButton){
+        print("Login Clicked")
+    }
     
     
 }
