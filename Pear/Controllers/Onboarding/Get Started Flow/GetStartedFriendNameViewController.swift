3b4448e2bffa39ad5aa9d97845a9ac5b94e079e6
Avery Lamp <averylamp@gmail.com>
2019-02-09 16:49:14 -0500

    Made verbose classes for each page and setup name fields

--- Pear/Controllers/Onboarding/Get Started Flow/GetStartedFriendNameViewController.swift
+++ Pear/Controllers/Onboarding/Get Started Flow/GetStartedFriendNameViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class GetStartedFriendNameViewController: UIViewController {
     
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    var endorsement: Endorsement!
+    
     /// Factory method for creating this view controller.
     ///
     /// - Returns: Returns an instance of this view controller.
-    class func instantiate() -> GetStartedFriendNameViewController {
+    class func instantiate(endorsement: Endorsement) -> GetStartedFriendNameViewController {
         let storyboard = UIStoryboard(name: String(describing: GetStartedFriendNameViewController.self), bundle: nil)
         let vc = storyboard.instantiateInitialViewController() as! GetStartedFriendNameViewController
+        vc.endorsement = endorsement
         return vc
     }
+    
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        if let userName = inputTextField.text{
+            endorsement.name = userName
+            let friendNameVC = GetStartedFriendNameViewController.instantiate(endorsement: self.endorsement)
+            self.navigationController?.pushViewController(friendNameVC, animated: true)
+        }
+    }
+    
 }
 
 
@@ -25,8 +45,11 @@ class GetStartedFriendNameViewController: UIViewController {
 extension GetStartedFriendNameViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        self.nextButton.contentMode = .scaleAspectFit
+        self.nextButton.layer.shadowRadius = 3
+        self.nextButton.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.nextButton.layer.shadowOpacity = 0.4
+        self.nextButton.layer.shadowColor = UIColor.black.cgColor
     }
     
 }
--- Pear/Controllers/Onboarding/Get Started Flow/GetStartedYourNameViewController.swift
+++ Pear/Controllers/Onboarding/Get Started Flow/GetStartedYourNameViewController.swift
@@ -10,23 +10,46 @@ import UIKit
 
 class GetStartedYourNameViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    var endorsement: Endorsement!
+    
     /// Factory method for creating this view controller.
     ///
     /// - Returns: Returns an instance of this view controller.
-    class func instantiate() -> GetStartedYourNameViewController {
+    class func instantiate(endorsement: Endorsement) -> GetStartedYourNameViewController {
         let storyboard = UIStoryboard(name: String(describing: GetStartedYourNameViewController.self), bundle: nil)
         let vc = storyboard.instantiateInitialViewController() as! GetStartedYourNameViewController
+        vc.endorsement = endorsement
         return vc
     }
+    
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        if let userName = inputTextField.text{
+            endorsement.endorseeName = userName
+            let friendNameVC = GetStartedFriendNameViewController.instantiate(endorsement: self.endorsement)
+            self.navigationController?.pushViewController(friendNameVC, animated: true)
+        }
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
-
 
 // MARK: - Life Cycle
 extension GetStartedYourNameViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        self.nextButton.contentMode = .scaleAspectFit
+        self.nextButton.layer.shadowRadius = 3
+        self.nextButton.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.nextButton.layer.shadowOpacity = 0.4
+        self.nextButton.layer.shadowColor = UIColor.black.cgColor
     }
     
 }
