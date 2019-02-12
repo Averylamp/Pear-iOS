4d9fc6c11201e6ff9e2cdeca2ab36d64f7d37a64
Avery Lamp <averylamp@gmail.com>
2019-02-11 20:22:50 -0500

    Do/Dont Stack view in Scroll view setup

--- Pear/Controllers/Onboarding/Get Started Flow/GetStartedDoDontViewController.swift
+++ Pear/Controllers/Onboarding/Get Started Flow/GetStartedDoDontViewController.swift
@@ -12,14 +12,15 @@ import UIKit
 class GetStartedDoDontViewController: UIViewController {
     
     var endorsement: Endorsement!
-    
-    @IBOutlet weak var inputTextViewHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var textLengthLabel: UILabel!
-    
-    @IBOutlet weak var inputTextView: UITextView!
-    
+        
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var samplesButtonWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var samplesButton: UIButton!
-    let maxTextLength: Int = 600
+    
+    let doDontTitleHeight: CGFloat = 34
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
     
     /// Factory method for creating this view controller.
     ///
@@ -38,8 +39,8 @@ class GetStartedDoDontViewController: UIViewController {
     @IBAction func nextButtonClicked(_ sender: Any) {
         HapticFeedbackGenerator.generateHapticFeedback(style: .light)
         
-        let photoInputVC = GetStartedPhotoInputViewController.instantiate(endorsement: self.endorsement)
-        self.navigationController?.pushViewController(photoInputVC, animated: true)
+        let createAccountVC = GetStartedCreateAccountViewController.instantiate(endorsement: self.endorsement)
+        self.navigationController?.pushViewController(createAccountVC, animated: true)
         
     }
     
@@ -58,41 +59,83 @@ class GetStartedDoDontViewController: UIViewController {
 extension GetStartedDoDontViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.inputTextView.delegate = self
-        self.inputTextView.becomeFirstResponder()
-        self.inputTextView.textContainerInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        self.inputTextView.isScrollEnabled = false
-        
-        self.textLengthLabel.text = "\(self.inputTextView.text.count) / \(maxTextLength)"
         
         self.samplesButton.layer.borderWidth = 1
         self.samplesButton.layer.borderColor = UIColor(red:0.07, green:0.07, blue:0.07, alpha:1.00).cgColor
         self.samplesButton.layer.cornerRadius = 20.0
-        self.samplesButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        self.view.layoutIfNeeded()
+        let titleWidth = self.samplesButton.titleLabel!.frame.width
+        let starsImage = UIImage(named: "onboarding-icon-samples-stars")
+        self.samplesButton.setImage(starsImage?.imageWith(newSize: CGSize(width: 20, height: 20)), for: .normal)
+        self.samplesButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 8)
+        self.samplesButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 0)
+        let imageWidth = self.samplesButton.imageView!.frame.width
+        let fullSamplesButtonWidth: CGFloat = titleWidth + imageWidth + 40
+        print("Width: \(fullSamplesButtonWidth)")
+        self.samplesButtonWidthConstraint.constant = fullSamplesButtonWidth
+        self.view.layoutIfNeeded()
         self.samplesButton.addMotionEffect(MotionEffectGroupGenerator.getMotionEffectGroup(maxDistance: 3.0))
+        
+        for i in 0...20{t
+            self.addDoDontGroup()
+        }
     }
     
-}
+    enum DoType {
+        case doType
+        case dontType
+    }
 
-// MARK: - UITextView Delegate
-extension GetStartedDoDontViewController:UITextViewDelegate {
     
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if textView.text.count + text.count - range.length > maxTextLength {
-            return false
+    func generateTitleLabel(type: DoType) -> UIView {
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: self.stackView.frame.width, height: 34))
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let thumbImageView = UIImageView(frame: CGRect(x: 20, y: 4, width: 26, height: 26))
+        if type == .doType{
+            thumbImageView.image = UIImage(named: "onboarding-icon-thumb-do")
+        }else{
+            thumbImageView.image = UIImage(named: "onboarding-icon-thumb-dont")
         }
-        return true
+        thumbImageView.contentMode = .scaleAspectFit
+        titleView.addSubview(thumbImageView)
+        
+        let textLabel = UILabel(frame: CGRect(x: 50, y: 0, width: titleView.frame.width - 60, height: titleView.frame.height))
+        textLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        if type == .doType{
+            textLabel.text = "Do..."
+        }else{
+            textLabel.text = "Don't..."
+        }
+        
+        titleView.addSubview(textLabel)
+        return titleView
     }
     
-    func textViewDidChange(_ textView: UITextView) {
-        self.textLengthLabel.text = "\(textView.text.count) / \(maxTextLength)"
-        let textHeight = self.inputTextView.sizeThatFits(CGSize(width: self.inputTextView.frame.width, height: CGFloat.greatestFiniteMagnitude)).height
-        let textPadding: CGFloat = 8.0
-        inputTextViewHeightConstraint.constant = max(34, textPadding + textHeight)
-        UIView.animate(withDuration: 0.3) {
-            self.view.layoutIfNeeded()
-        }
+    
+    
+    
+    func addDoDontGroup(){
+        let doTitleView = generateTitleLabel(type: .doType)
+        self.stackView.addArrangedSubview(doTitleView)
+        self.stackView.addConstraints([
+                NSLayoutConstraint(item: doTitleView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: doDontTitleHeight),
+                NSLayoutConstraint(item: doTitleView, attribute: .width, relatedBy: .equal, toItem: self.stackView, attribute: .width, multiplier: 1.0, constant: 0)
+            ])
+        
+        
+        
+        let dontTitleView = generateTitleLabel(type: .dontType)
+        self.stackView.addArrangedSubview(dontTitleView)
+        self.stackView.addConstraints([
+            NSLayoutConstraint(item: dontTitleView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: doDontTitleHeight),
+            NSLayoutConstraint(item: dontTitleView, attribute: .width, relatedBy: .equal, toItem: self.stackView, attribute: .width, multiplier: 1.0, constant: 0)
+            ])
         
     }
     
+    func removeLastDoDontGroup(){
+        
+        
+    }
 }
