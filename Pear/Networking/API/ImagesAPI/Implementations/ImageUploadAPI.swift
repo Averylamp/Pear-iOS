674eb3905a05f31144d18837a4b8440c0c0768ec
Avery Lamp <averylamp@gmail.com>
2019-02-23 06:39:58 -0500

    Simple image upload api that sends images

--- Pear/Networking/API/ImagesAPI/Implementations/ImageUploadAPI.swift
+++ Pear/Networking/API/ImagesAPI/Implementations/ImageUploadAPI.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import Foundation
 
 class ImageUploadAPI:ImageAPI  {
+    
+    static let shared = ImageUploadAPI()
+    
+    private init(){
+        
+    }
+    
     func uploadNewImage(with image: UIImage, completion: @escaping (Result<ImageAllSizesRepresentation, ImageAPIError>) -> Void) {
         
         
+        let headers:[String: String] = [
+            "Content-Type": "application/json",
+//            "Content-Type": "application/x-www-form-urlencoded",
+        ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "\(Config.imageAPIHost)/upload_image")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 15.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        let imageString: String = image.jpegData(compressionQuality: 0.8)!.base64EncodedString()
+        do {
+            
+            request.httpBody = try JSONSerialization.data(withJSONObject: ["image": imageString], options: .prettyPrinted)
+            
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                if (error != nil) {
+                    print(error)
+                } else {
+                    let httpResponse = response as? HTTPURLResponse
+                    print(httpResponse)
+                }
+            })
+            
+            dataTask.resume()
+        }catch{
+//            completion( .failure(error))
+        }
         
         
     }
