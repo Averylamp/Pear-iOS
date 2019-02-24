2e94bb3c77f263d3295ed0faac02e0f137572284
Avery Lamp <averylamp@gmail.com>
2019-02-24 15:54:45 -0500

    Added Nunito font

--- Pear/Shared/Config.swift
+++ Pear/Shared/Config.swift
@@ -14,6 +14,14 @@ class Config{
     
     static let imageAPIHost: URL = URL(string: "http://koala.mit.edu:1337")!
     
+    static let textFontRegular: String = "Nunita-Regular"
+    static let textFontBlack: String = "Nunita-Black"
+    static let textFontBold: String = "Nunita-Bold"
+    static let textFontExtraBold: String = "Nunita-ExtraBold"
+    static let textFontExtraLight: String = "Nunita-ExtraLight"
+    static let textFontLight: String = "Nunita-Light"
+    static let textFontSemiBold: String = "Nunita-SemiBold"
+    
     
     
 }
