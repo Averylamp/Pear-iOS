ecd5844548b6355f9b3bc04372cbc8dce0eb37af
Avery Lamp <averylamp@gmail.com>
2019-02-25 01:23:10 -0500

    Added kerning to pear labels

---
+++ Pear/Shared/UILabelExtensions.swift
@@ -0,0 +1,40 @@
+//
+//  UILabelExtensions.swift
+//  Pear
+//
+//  Created by Avery Lamp on 2/25/19.
+//  Copyright © 2019 sam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    @IBInspectable var kerning: Float {
+        get {
+            var range = NSMakeRange(0, (text ?? "").count)
+            guard let kern = attributedText?.attribute(NSAttributedString.Key.kern, at: 0, effectiveRange: &range),
+                let value = kern as? NSNumber
+                else {
+                    return 0
+            }
+            return value.floatValue
+        }
+        set {
+            var attText:NSMutableAttributedString
+            
+            if let attributedText = attributedText {
+                attText = NSMutableAttributedString(attributedString: attributedText)
+            } else if let text = text {
+                attText = NSMutableAttributedString(string: text)
+            } else {
+                attText = NSMutableAttributedString(string: "")
+            }
+            
+            let range = NSMakeRange(0, attText.length)
+            attText.addAttribute(NSAttributedString.Key.kern, value: NSNumber(value: newValue), range: range)
+            self.attributedText = attText
+        }
+    }
+}
