54a62765800ff71ece16a8fc368c3cda4b745adb
Avery Lamp <averylamp@gmail.com>
2019-02-25 16:37:40 -0500

    Added endorsee first name

--- Pear/Controllers/Onboarding/Get Started Flow/Endorsement Name Input/GetStartedFriendNameViewController.swift
+++ Pear/Controllers/Onboarding/Get Started Flow/Endorsement Name Input/GetStartedFriendNameViewController.swift
@@ -14,6 +14,10 @@ class GetStartedFriendNameViewController: UIViewController {
     @IBOutlet weak var inputTextField: UITextField!
     
     var gettingStartedData: GetttingStartedData!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var nextButtonBottomConstraint: NSLayoutConstraint!
+    
     
     /// Factory method for creating this view controller.
     ///
@@ -25,31 +29,25 @@ class GetStartedFriendNameViewController: UIViewController {
         return vc
     }
     
-    
-    @IBAction func backButtonClicked(_ sender: Any) {
-        if let userName = inputTextField.text{
-            self.gettingStartedData.profileData.fullName = userName
-            let splitNames = userName.splitIntoFirstLastName()
-            self.gettingStartedData.profileData.firstName = splitNames.0
-            self.gettingStartedData.profileData.lastName = splitNames.1
-        }
-        self.navigationController?.popViewController(animated: true)
-    }
-    
     @IBAction func nextButtonClicked(_ sender: Any) {
         HapticFeedbackGenerator.generateHapticFeedbackImpact(style: .light)
-        if let userName = inputTextField.text{
-            self.gettingStartedData.profileData.fullName = userName
-            let splitNames = userName.splitIntoFirstLastName()
-            self.gettingStartedData.profileData.firstName = splitNames.0
-            self.gettingStartedData.profileData.lastName = splitNames.1
-            if splitNames.0.count < 1{
+        if let endorseeFirstName = inputTextField.text{
+            self.gettingStartedData.profileData.endorsedFirstName = endorseeFirstName
+            if endorseeFirstName.count < 1{
                 self.alert(title: "Name Missing", message: "Please enter your name")
                 return
             }
-            let friendNameVC = GetStartedAgeViewController.instantiate(gettingStartedData: self.gettingStartedData)
-            self.navigationController?.pushViewController(friendNameVC, animated: true)
+            
+            let phoneVerificationVC = GetStartedValidatePhoneNumberViewController.instantiate(gettingStartedData: self.gettingStartedData)
+            self.navigationController?.pushViewController(phoneVerificationVC, animated: true)
         }
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        if let userName = inputTextField.text{
+            self.gettingStartedData.profileData.endorsedFullName = userName
+        }
+        self.navigationController?.popViewController(animated: true)
     }
     
     @IBAction func clearButtonClicked(_ sender: Any) {
@@ -57,14 +55,63 @@ class GetStartedFriendNameViewController: UIViewController {
     }
 }
 
-
 // MARK: - Life Cycle
 extension GetStartedFriendNameViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.nextButton.contentMode = .scaleAspectFit
-        self.inputTextField.text = self.gettingStartedData.profileData.fullName
+        if let endorseeFirst = self.gettingStartedData.profileData.endorsedFirstName{
+            self.inputTextField.text = endorseeFirst
+        }
         self.inputTextField.becomeFirstResponder()
+        self.styleViews()
+        self.addKeyboardSizeNotifications()
+    }
+    
+    func styleViews(){
+        self.titleLabel.font = UIFont(name: Config.displayFontRegular, size: 28)
+        self.titleLabel.textColor = Config.textFontColor
+        
+        self.nextButton.backgroundColor = Config.nextButtonColor
+        self.nextButton.layer.cornerRadius = self.nextButton.frame.height / 2.0
+        self.nextButton.layer.shadowRadius = 1
+        self.nextButton.layer.shadowColor = UIColor(white: 0.0, alpha: 0.15).cgColor
+        self.nextButton.layer.shadowOffset = CGSize(width: 0, height: 1)
+        self.nextButton.layer.shadowOpacity = 1.0
+        self.nextButton.setTitleColor(UIColor.white, for: .normal)
+        self.nextButton.titleLabel?.font = UIFont(name: Config.textFontSemiBold, size: 17)
+        
+    }
+    
+    func addKeyboardSizeNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(GetStartedFriendNameViewController.keyboardWillChange(notification:)), name: UIWindow.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(GetStartedFriendNameViewController.keyboardWillHide(notification:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+}
+
+// MARK: - Keybaord Size Notifications
+extension GetStartedFriendNameViewController{
+    
+    @objc func keyboardWillChange(notification: Notification){
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let targetFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let bottomSpacing: CGFloat = 20
+        self.nextButtonBottomConstraint.constant = targetFrame.size.height - self.view.safeAreaInsets.bottom + bottomSpacing
+        UIView.animate(withDuration: duration) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification){
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        self.nextButtonBottomConstraint.constant = 0
+        UIView.animate(withDuration: duration) {
+            self.view.layoutIfNeeded()
+        }
     }
     
 }
+
