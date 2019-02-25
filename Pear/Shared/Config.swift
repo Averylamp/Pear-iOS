104126c48c23a563dadbb2f0e7eda873b4f49b5c
Avery Lamp <averylamp@gmail.com>
2019-02-25 15:40:43 -0500

    Added pick path VC, Cherry Pick samples VC,

--- Pear/Shared/Config.swift
+++ Pear/Shared/Config.swift
@@ -28,6 +28,8 @@ class Config{
     static let textFontSemiBold: String = "Nunito-SemiBold"
     static let textFontColor: UIColor = UIColor(red:0.27, green:0.29, blue:0.33, alpha:1.00)
     
+    static let inactiveTextFontColor: UIColor = UIColor(red:0.27, green:0.29, blue:0.33, alpha:0.3)
+    
     static let nextButtonColor: UIColor = UIColor(red:0.27, green:0.29, blue:0.33, alpha:1.00)
     
     
