1e42444476b17ce3126bce1d8cdabfc93ad77e79
Avery Lamp <averylamp@gmail.com>
2019-02-25 01:31:48 -0500

    Fixed styling for landing

--- Pear/Controllers/Onboarding/Landing Screen/Landing Screen View Controller/LandingScreenViewController.swift
+++ Pear/Controllers/Onboarding/Landing Screen/Landing Screen View Controller/LandingScreenViewController.swift
@@ -57,14 +57,20 @@ extension LandingScreenViewController{
         signupButton.layer.shadowOffset = CGSize(width: 1, height: 1)
         signupButton.layer.shadowColor = UIColor.black.cgColor
         signupButton.layer.shadowOpacity = 0.4
-        signupButton.layer.cornerRadius = 4
+        signupButton.layer.cornerRadius = self.signupButton.frame.height / 2
+        signupButton.setTitleColor(UIColor.white, for: .normal)
+        signupButton.layer.backgroundColor = Config.textFontColor.cgColor
+        signupButton.titleLabel?.font = UIFont(name: Config.textFontExtraBold, size: 18)
         signupButton.addMotionEffect(MotionEffectGroupGenerator.getMotionEffectGroup(maxDistance: 3.0))
         
         loginButton.layer.shadowRadius = 1
         loginButton.layer.shadowOffset = CGSize(width: 1, height: 1)
         loginButton.layer.shadowColor = UIColor.black.cgColor
         loginButton.layer.shadowOpacity = 0.4
-        loginButton.layer.cornerRadius = 4
+        loginButton.layer.cornerRadius = self.loginButton.frame.height / 2
+        loginButton.setTitleColor(Config.textFontColor, for: .normal)
+        loginButton.layer.backgroundColor = UIColor.white.cgColor
+        loginButton.titleLabel?.font = UIFont(name: Config.textFontExtraBold, size: 18)
         loginButton.addMotionEffect(MotionEffectGroupGenerator.getMotionEffectGroup(maxDistance: 3.0))
     }
     
