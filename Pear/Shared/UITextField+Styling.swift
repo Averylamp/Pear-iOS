f8f4336f265ce907e06ac0b344c08d4010f05eb4
Avery Lamp <averylamp@gmail.com>
2019-02-28 04:34:32 -0500

    Added new Text Field type/styling

---
+++ Pear/Shared/UITextField+Styling.swift
@@ -0,0 +1,20 @@
+//
+//  UITextField+Styling.swift
+//  Pear
+//
+//  Created by Avery Lamp on 2/28/19.
+//  Copyright © 2019 sam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+
+    func stylizeGettingStarted() {
+        self.font = UIFont(name: Config.displayFontRegular, size: 18)
+        self.textColor = Config.textFontColor
+        self.tintColor = Config.textFontColor
+    }
+
+}
