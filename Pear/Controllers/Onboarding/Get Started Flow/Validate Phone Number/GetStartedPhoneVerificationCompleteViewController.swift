e80f21d5c479906291ee4f2bc8ce073aaf6e38ee
Avery Lamp <averylamp@gmail.com>
2019-02-28 03:51:03 -0500

    Added verified screen

---
+++ Pear/Controllers/Onboarding/Get Started Flow/Validate Phone Number/GetStartedPhoneVerificationCompleteViewController.swift
@@ -0,0 +1,89 @@
+//
+//  GetStartedPhoneVerificationCompleteViewController.swift
+//  Pear
+//
+//  Created by Avery Lamp on 2/28/19.
+//  Copyright © 2019 sam. All rights reserved.
+//
+
+import UIKit
+
+class GetStartedPhoneVerificationCompleteViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var nextButtonBottomConstraint: NSLayoutConstraint!
+
+    /// Factory method for creating this view controller.
+    ///
+    /// - Returns: Returns an instance of this view controller.
+    class func instantiate() -> GetStartedPhoneVerificationCompleteViewController? {
+        let storyboard = UIStoryboard(name: String(describing: GetStartedPhoneVerificationCompleteViewController.self), bundle: nil)
+        guard let phoneVerificationCompleteVC = storyboard.instantiateInitialViewController() as? GetStartedPhoneVerificationCompleteViewController else { return nil }
+        return phoneVerificationCompleteVC
+    }
+
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        HapticFeedbackGenerator.generateHapticFeedbackImpact(style: .light)
+        guard let chooseFlowVC = GetStartedChooseFlowViewController.instantiate(gettingStartedData: GettingStartedData()) else {
+            print("Failed to create Choose flow VC")
+            return
+        }
+        self.navigationController?.setViewControllers([chooseFlowVC], animated: true)
+
+    }
+}
+
+// MARK: - Life Cycle
+extension GetStartedPhoneVerificationCompleteViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.addKeyboardSizeNotifications()
+    }
+
+}
+
+// MARK: - Keybaord Size Notifications
+extension GetStartedPhoneVerificationCompleteViewController {
+    func addKeyboardSizeNotifications() {
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(GetStartedPhoneVerificationCompleteViewController.keyboardWillChange(notification:)),
+                         name: UIWindow.keyboardWillChangeFrameNotification,
+                         object: nil)
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(GetStartedPhoneVerificationCompleteViewController.keyboardWillHide(notification:)),
+                         name: UIWindow.keyboardWillHideNotification,
+                         object: nil)
+    }
+
+    @objc func keyboardWillChange(notification: Notification) {
+        if let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+            let targetFrameNSValue = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let targetFrame = targetFrameNSValue.cgRectValue
+            let keyboardBottomPadding: CGFloat = 20
+            self.nextButtonBottomConstraint.constant = targetFrame.size.height - self.view.safeAreaInsets.bottom + keyboardBottomPadding
+            UIView.animate(withDuration: duration) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    @objc func keyboardWillHide(notification: Notification) {
+        if let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? Double {
+            let keyboardBottomPadding: CGFloat = 20
+            self.nextButtonBottomConstraint.constant = keyboardBottomPadding
+            UIView.animate(withDuration: duration) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+
+}
