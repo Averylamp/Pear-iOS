ce17d3e65a68f3079dda1361623f294f698b2bba
Avery Lamp <averylamp@gmail.com>
2019-03-01 15:34:54 -0500

    Styling

---
+++ Pear/Shared/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  Pear
+//
+//  Created by Avery Lamp on 2/28/19.
+//  Copyright © 2019 sam. All rights reserved.
+//
+
+import UIKit
+
+class Colors {
+
+    /// Primary Brand Color Light
+    static let brandPrimaryLight: UIColor = UIColor(red: 0.12, green: 0.83, blue: 0.69, alpha: 1.00)
+
+    /// Primary Brand Color Dark
+    static let brandPrimaryDark: UIColor = UIColor(red: 0.00, green: 0.70, blue: 0.56, alpha: 1.00)
+
+    /// Shadow Color
+    static let shadowColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15)
+}
