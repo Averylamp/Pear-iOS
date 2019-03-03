baed65586984d00c410d2a3f80d04909c42f34b5
Avery Lamp <averylamp@gmail.com>
2019-03-03 04:36:45 -0500

    Added FB Login graph request to prefill data and modified login flow to reroute for unfilled data

--- Pear/Networking/Models/GettingStartedUserData.swift
+++ Pear/Networking/Models/GettingStartedUserData.swift
@@ -8,14 +8,16 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
-class GettingStartedUserData {
+class GettingStartedUserData: CustomStringConvertible {
 
     var firstName: String?
     var lastName: String?
     var email: String?
     var emailVerified: Bool = false
     var age: Int?
+    var gender: GenderEnum?
     var birthdate: Date?
     var phoneNumber: String?
     var phoneNumberVerified: Bool = false
@@ -40,6 +42,71 @@ class GettingStartedUserData {
 
     init() {
 
+    }
+    var description: String {
+        return "**** GettingStartedUserData\n " + """
+        firstName: \(String(describing: firstName)),
+        lastName: \(String(describing: lastName)),
+        email: \(String(describing: email)),
+        emailVerified: \(String(describing: emailVerified)),
+        age: \(String(describing: age)),
+        gender: \(String(describing: gender)),
+        birthdate: \(String(describing: birthdate)),
+        phoneNumber: \(String(describing: phoneNumber)),
+        phoneNumberVerified: \(String(describing: phoneNumberVerified)),
+        firebaseToken: \(String(describing: firebaseToken)),
+        firebaseAuthID: \(String(describing: firebaseAuthID)),
+        facebookId: \(String(describing: facebookId)),
+        facebookAccessToken: \(String(describing: facebookAccessToken)),
+        """
+    }
+
+    func getNextInputViewController() -> UIViewController? {
+
+        if self.email == nil {
+            guard let emailInputVC = GetStartedEmailProviderViewController.instantiate(gettingStartedUserData: self) else {
+                print("Failed to create Email Provider VC")
+                return nil
+            }
+            return emailInputVC
+        }
+
+        if self.firstName == nil {
+            guard let userFirstNameVC = GetStartedUserFirstNameViewController.instantiate(gettingStartedUserData: self) else {
+                print("Failed to start User First Name VC: ")
+                return nil
+            }
+            return userFirstNameVC
+        }
+
+        if self.lastName == nil {
+            guard let userLastNameVC = GetStartedUserLastNameViewController.instantiate(gettingStartedUserData: self) else {
+                print("Failed to start User Last Name VC: ")
+                return nil
+            }
+            return userLastNameVC
+        }
+
+        if self.age  == nil {
+            print("User Åge Missing")
+            print("RED FLAGSS")
+            self.age = 21
+        }
+
+        if self.birthdate  == nil {
+            print("User Birthday Missing")
+            print("RED FLAGSS")
+        }
+
+        if self.phoneNumber == nil {
+            guard let phoneInputVC = GetStartedValidatePhoneNumberViewController.instantiate(gettingStartedUserData: self) else {
+                print("Failed to create Phone Number VC")
+                return nil
+            }
+            return phoneInputVC
+        }
+
+        return nil
     }
 
 }
