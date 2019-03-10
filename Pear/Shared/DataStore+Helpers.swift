f798520edfaa0416079f5e66513cda759cd6c90c
Avery Lamp <averylamp@gmail.com>
2019-03-10 03:44:12 -0400

    Added helpers to fetch User obj efficiently.  Also deserializes DetachedProfiles properly

---
+++ Pear/Shared/DataStore+Helpers.swift
@@ -0,0 +1,48 @@
+//
+//  DataStore+Helpers.swift
+//  Pear
+//
+//  Created by Avery Lamp on 3/10/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+extension DataStore {
+  
+  func checkForExistingUser(pearUserFoundCompletion: @escaping () -> Void, userNotFoundCompletion: @escaping () -> Void) {
+    if let currentUser = Auth.auth().currentUser {
+      let uid = currentUser.uid
+      currentUser.getIDToken { (token, error) in
+        if let error = error {
+          print("Error getting token: \(error)")
+          userNotFoundCompletion()
+          return
+        }
+        if let token = token {
+          PearUserAPI.shared.getUser(uid: uid,
+                                     token: token,
+                                     completion: { (result) in
+              switch result {
+              case .success(let pearUser):
+                print("Got Existing Pear User \(String(describing: pearUser))")
+                DataStore.shared.currentPearUser = pearUser
+                pearUserFoundCompletion()
+              case .failure(let error):
+                print("Error getting Pear User: \(error)")
+                userNotFoundCompletion()
+              }
+          })
+          
+        } else {
+          print("No token found")
+          userNotFoundCompletion()
+        }
+      }
+    } else {
+      userNotFoundCompletion()
+    }
+  }
+  
+}
