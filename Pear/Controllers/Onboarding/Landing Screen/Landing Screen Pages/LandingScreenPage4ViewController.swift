9fd7b5dd4e7e66542ee102f6c6603e55daf47bf2
Avery Lamp <averylamp@gmail.com>
2019-03-10 01:57:37 -0500

    Reindent Landing screen pages

--- Pear/Controllers/Onboarding/Landing Screen/Landing Screen Pages/LandingScreenPage4ViewController.swift
+++ Pear/Controllers/Onboarding/Landing Screen/Landing Screen Pages/LandingScreenPage4ViewController.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class LandingScreenPage4ViewController: LandingScreenPageViewController {
-
-    /// Factory method for creating this view controller.
-    ///
-    /// - Returns: Returns an instance of this view controller.
-    class func instantiate() -> LandingScreenPage4ViewController? {
-        let storyboard = UIStoryboard(name: String(describing: LandingScreenPage4ViewController.self), bundle: nil)
-        guard let page4VC = storyboard.instantiateInitialViewController() as? LandingScreenPage4ViewController else { return nil }
-        return page4VC
-    }
-
+  
+  /// Factory method for creating this view controller.
+  ///
+  /// - Returns: Returns an instance of this view controller.
+  class func instantiate() -> LandingScreenPage4ViewController? {
+    let storyboard = UIStoryboard(name: String(describing: LandingScreenPage4ViewController.self), bundle: nil)
+    guard let page4VC = storyboard.instantiateInitialViewController() as? LandingScreenPage4ViewController else { return nil }
+    return page4VC
+  }
+  
 }
 
 // MARK: - Life Cycle
 extension LandingScreenPage4ViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
 }
