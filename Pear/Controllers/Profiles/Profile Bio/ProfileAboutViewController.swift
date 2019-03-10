4c832090a35989c2c894d99d5620ed26e5e40766
Avery Lamp <averylamp@gmail.com>
2019-03-10 01:56:45 -0500

    Reindented old profile code

--- Pear/Controllers/Profiles/Profile Bio/ProfileAboutViewController.swift
+++ Pear/Controllers/Profiles/Profile Bio/ProfileAboutViewController.swift
@@ -9,33 +9,33 @@
 import UIKit
 
 class ProfileAboutViewController: UIViewController {
-
-    var aboutBio: String!
-    @IBOutlet weak var bioLabel: UILabel!
-
-    var profileBioHeightConstraint: NSLayoutConstraint?
-    /// Factory method for creating this view controller.
-    ///
-    /// - Returns: Returns an instance of this view controller.
-    class func instantiate(aboutBio: String) -> ProfileAboutViewController? {
-        let storyboard = UIStoryboard(name: String(describing: ProfileAboutViewController.self), bundle: nil)
-        guard let profileAboutVC = storyboard.instantiateInitialViewController() as? ProfileAboutViewController else { return nil }
-        profileAboutVC.aboutBio = aboutBio
-        return profileAboutVC
-    }
-
+  
+  var aboutBio: String!
+  @IBOutlet weak var bioLabel: UILabel!
+  
+  var profileBioHeightConstraint: NSLayoutConstraint?
+  /// Factory method for creating this view controller.
+  ///
+  /// - Returns: Returns an instance of this view controller.
+  class func instantiate(aboutBio: String) -> ProfileAboutViewController? {
+    let storyboard = UIStoryboard(name: String(describing: ProfileAboutViewController.self), bundle: nil)
+    guard let profileAboutVC = storyboard.instantiateInitialViewController() as? ProfileAboutViewController else { return nil }
+    profileAboutVC.aboutBio = aboutBio
+    return profileAboutVC
+  }
+  
 }
 
 // MARK: - Life Cycle
 extension ProfileAboutViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.bioLabel.text = aboutBio
-    }
-
-    func setHeightConstraint(constraint: NSLayoutConstraint) {
-        self.profileBioHeightConstraint = constraint
-        self.view.layoutIfNeeded()
-        constraint.constant = self.bioLabel.frame.origin.y + self.bioLabel.frame.height + 12
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.bioLabel.text = aboutBio
+  }
+  
+  func setHeightConstraint(constraint: NSLayoutConstraint) {
+    self.profileBioHeightConstraint = constraint
+    self.view.layoutIfNeeded()
+    constraint.constant = self.bioLabel.frame.origin.y + self.bioLabel.frame.height + 12
+  }
 }
