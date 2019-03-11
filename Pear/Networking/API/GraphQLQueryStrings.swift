7310889775564df45e5853e6553c44d269f3b5f9
Avery Lamp <averylamp@gmail.com>
2019-03-11 17:42:55 -0400

    Image schema patch

--- Pear/Networking/API/GraphQLQueryStrings.swift
+++ Pear/Networking/API/GraphQLQueryStrings.swift
@@ -13,7 +13,4 @@ class GraphQLQueryStrings {
   static let matchingDemographicsFull: String = "{ethnicities religion political smoking drinking height}"
   // swiftlint:disable:next line_length
   static let matchingPreferencesFull: String = "{ethnicities seekingGender seekingReason reasonDealbreaker seekingEthnicity ethnicityDealbreaker maxDistance distanceDealbreaker minAgeRange maxAgeRange ageDealbreaker minHeightRange maxHeightRange heightDealbreaker }"
-  static let imageSizesFull: String = "{original \(GraphQLQueryStrings.imageMetadataFull) large \(GraphQLQueryStrings.imageMetadataFull) medium \(GraphQLQueryStrings.imageMetadataFull) small \(GraphQLQueryStrings.imageMetadataFull) thumbnail \(GraphQLQueryStrings.imageMetadataFull)}"
-  
-  static let imageMetadataFull: String = "{imageURL imageID imageSize {width height}}"
 }
