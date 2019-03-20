42a3e6f56b0a0e0228c457f7a9a44aa2095bf161
Avery Lamp <averylamp@gmail.com>
2019-03-19 20:39:11 -0400

    Added dev mode

---
+++ Pear/Shared/DevConfig.swift
@@ -0,0 +1,15 @@
+//
+//  DevConfig.swift
+//  Pear
+//
+//  Created by Avery Lamp on 3/19/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+
+class DevConfig {
+  
+  static let devMode: Bool = true
+  
+}
