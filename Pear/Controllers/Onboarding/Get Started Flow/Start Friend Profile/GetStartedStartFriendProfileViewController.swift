76bcdda87ca8f84856c1f9f9b3ac83ebc301290f
Avery Lamp <averylamp@gmail.com>
2019-03-19 20:39:11 -0400

    Added dev mode

--- Pear/Controllers/Onboarding/Get Started Flow/Start Friend Profile/GetStartedStartFriendProfileViewController.swift
+++ Pear/Controllers/Onboarding/Get Started Flow/Start Friend Profile/GetStartedStartFriendProfileViewController.swift
@@ -30,7 +30,9 @@ class GetStartedStartFriendProfileViewController: UIViewController {
     let storyboard = UIStoryboard(name: String(describing: GetStartedStartFriendProfileViewController.self), bundle: nil)
     guard let startFriendProfileVC = storyboard.instantiateInitialViewController() as? GetStartedStartFriendProfileViewController else { return nil }
     startFriendProfileVC.gettingStartedData = GettingStartedUserProfileData()
-//    startFriendProfileVC.gettingStartedData.fakePopulate()
+    if DevConfig.devMode {
+      startFriendProfileVC.gettingStartedData.fakePopulate()
+    }
     return startFriendProfileVC
   }
   
