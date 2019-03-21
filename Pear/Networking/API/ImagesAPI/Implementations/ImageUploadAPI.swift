a236e14a7272089b43d779bbbea2120a18c35670
Avery Lamp <averylamp@gmail.com>
2019-03-21 04:34:29 -0400

    Added more traces

--- Pear/Networking/API/ImagesAPI/Implementations/ImageUploadAPI.swift
+++ Pear/Networking/API/ImagesAPI/Implementations/ImageUploadAPI.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Foundation
 import SwiftyJSON
+import FirebasePerformance
 
 class ImageUploadAPI: ImageAPI {
   
@@ -19,7 +20,7 @@ class ImageUploadAPI: ImageAPI {
   }
   
   func uploadNewImage(with image: UIImage, userID: String, completion: @escaping (Result<ImageContainer, ImageAPIError>) -> Void) {
-    
+    let trace = Performance.startTrace(name: "Image Upload")
     var scaleRatio: CGFloat = 1.0
     if image.size.width > image.size.height {
       scaleRatio = image.size.height > 1700 ? 1700 / image.size.height : scaleRatio
@@ -29,6 +30,7 @@ class ImageUploadAPI: ImageAPI {
     
     let finalImage: UIImage!
     if scaleRatio < 1.0 {
+      trace?.incrementMetric("Image Resized", by: 1)
       finalImage = image.imageWith(newSize: CGSize(width: image.size.width * scaleRatio,
                                                   height: image.size.height * scaleRatio))
     } else {
@@ -56,17 +58,26 @@ class ImageUploadAPI: ImageAPI {
         .dataTask(with: request as URLRequest, completionHandler: { (data, _, error) -> Void in
           if let error = error {
             print(error as Any)
+            trace?.incrementMetric("Image Upload Error", by: 1)
+            trace?.stop()
             completion(.failure(ImageAPIError.unknownError(error: error)))
           } else {
+            trace?.incrementMetric("Image Request Successful", by: 1)
             if let data = data {
               do {
                 let imageContainer = try JSONDecoder().decode(ImageContainer.self, from: data)
                 imageContainer.uploadedByUser = userID
+                trace?.incrementMetric("Image Decoding Successful", by: 1)
+                trace?.stop()
                 completion(.success(imageContainer))
               } catch {
+                trace?.incrementMetric("Image Decoding Unsuccessful", by: 1)
+                trace?.stop()
                 completion(.failure(ImageAPIError.unknownError(error: error)))
               }
             } else {
+              trace?.incrementMetric("Image Request Data Missing", by: 1)
+              trace?.stop()
               completion(.failure(ImageAPIError.failedDeserialization))
             }
           }
--- Pear/Shared/DataStore+Helpers.swift
+++ Pear/Shared/DataStore+Helpers.swift
@@ -8,19 +8,24 @@
 
 import Foundation
 import FirebaseAuth
+import FirebasePerformance
 
 extension DataStore {
   
   func checkForExistingUser(pearUserFoundCompletion: @escaping () -> Void, userNotFoundCompletion: @escaping () -> Void) {
+    let trace = Performance.startTrace(name: "Loading Screen Existing User")
     if let currentUser = Auth.auth().currentUser {
       let uid = currentUser.uid
       currentUser.getIDToken { (token, error) in
         if let error = error {
-          print("Error getting token: \(error)")
+          print("Error getting firebase token: \(error)")
+          trace?.incrementMetric("Firebase Token Error", by: 1)
+          trace?.stop()
           userNotFoundCompletion()
           return
         }
         if let token = token {
+          trace?.incrementMetric("Firebase Token Found", by: 1)
           PearUserAPI.shared.getUser(uid: uid,
                                      token: token,
                                      completion: { (result) in
@@ -28,19 +33,28 @@ extension DataStore {
               case .success(let pearUser):
                 print("Got Existing Pear User \(String(describing: pearUser))")
                 DataStore.shared.currentPearUser = pearUser
+                trace?.incrementMetric("Existing User Found", by: 1)
+                trace?.stop()
                 pearUserFoundCompletion()
+                return
               case .failure(let error):
                 print("Error getting Pear User: \(error)")
+                trace?.incrementMetric("No Existing User Found", by: 1)
+                trace?.stop()
                 userNotFoundCompletion()
+                return
               }
           })
-          
         } else {
+          trace?.incrementMetric("Firebase Token Not Found", by: 1)
+          trace?.stop()
           print("No token found")
           userNotFoundCompletion()
         }
       }
     } else {
+      trace?.incrementMetric("Current Firebase User Not Found", by: 1)
+      trace?.stop()
       userNotFoundCompletion()
     }
   }
