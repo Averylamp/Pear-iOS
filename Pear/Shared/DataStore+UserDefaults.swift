9f7f0aed43d4d08b5ba5a4777f21b0b1144cf5c0
Avery Lamp <averylamp@gmail.com>
2019-03-30 01:09:28 -0400

    PP and EULA added to front page

---
+++ Pear/Shared/DataStore+UserDefaults.swift
@@ -0,0 +1,18 @@
+//
+//  DataStore+UserDefaults.swift
+//  Pear
+//
+//  Created by Avery Lamp on 3/30/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+
+extension DataStore {
+  
+  enum UserDefaultKeys: String {
+    case skippedDetachedProfiles
+    
+  }
+  
+}
