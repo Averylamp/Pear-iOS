c943c652f090f5aae5734150c5e76ecb3890f287
Avery Lamp <averylamp@gmail.com>
2019-04-01 00:58:54 -0400

    Adds indicator views fixes #38

--- Pear/Controllers/Discovery Flow/Simple Discovery/DiscoveryTableViewCell.swift
+++ Pear/Controllers/Discovery Flow/Simple Discovery/DiscoveryTableViewCell.swift
@@ -13,6 +13,7 @@ class DiscoveryTableViewCell: UITableViewCell {
   
   var profileData: FullProfileDisplayData!
   var imageViews: [UIImageView] = []
+  var indicatorViews: [UIView] = []
   var bioView: BioView!
   var doView: DoDontView!
   var dontView: DoDontView!
@@ -20,14 +21,23 @@ class DiscoveryTableViewCell: UITableViewCell {
   @IBOutlet weak var contentScrollView: UIScrollView!
   @IBOutlet weak var contentStackView: UIStackView!
   @IBOutlet weak var itemsStackView: UIStackView!
+  @IBOutlet weak var gradientView: UIView!
   
   let indentWidth: CGFloat = 20.0
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    print("Awake from nib called")
     self.setupContentViews()
     self.setupScrollView()
+    self.stylize()
+  }
+  
+  func stylize() {
+    let gradient = CAGradientLayer()
+    gradient.frame = CGRect(origin: CGPoint.zero, size: self.gradientView.frame.size)
+    gradient.colors = [UIColor(white: 0.0, alpha: 0.0).cgColor, UIColor(white: 0.0, alpha: 0.08).cgColor]
+    
+    self.gradientView.layer.insertSublayer(gradient, at: 0)
   }
   
   func setupContentViews() {
@@ -59,24 +69,31 @@ class DiscoveryTableViewCell: UITableViewCell {
   func setupScrollView() {
     self.contentScrollView.delegate = self
     self.contentScrollView.isPagingEnabled = true
+    self.contentScrollView.showsHorizontalScrollIndicator = false
   }
   
   func configureCell(profileData: FullProfileDisplayData) {
     self.profileData = profileData
+    self.contentScrollView.contentOffset = CGPoint.zero
+    self.indicatorViews.forEach({
+      $0.removeFromSuperview()
+    })
+    self.indicatorViews = []
+    
+    var pagesCount = 0
     for index in 0..<3 {
       let imageView = self.imageViews[index]
       imageView.image = nil
       if index < profileData.rawImages.count {
-        print("Raw Image")
         imageView.image = profileData.rawImages[index]
         imageView.isHidden = false
+        pagesCount += 1
       } else if index < profileData.imageContainers.count,
         let imageURL = URL(string: profileData.imageContainers[index].medium.imageURL) {
-        print("ImageURL")
         imageView.sd_setImage(with: imageURL, completed: nil)
         imageView.isHidden = false
+        pagesCount += 1
       } else {
-        print("Hiding image: \(index)")
         imageView.isHidden = true
       }
     }
@@ -84,6 +101,7 @@ class DiscoveryTableViewCell: UITableViewCell {
       self.bioView.isHidden = false
       self.bioView.writtenByLabel?.stylizeCreatorLabel(preText: "Written by ", boldText: bioContent.creatorName)
       self.bioView.contentLabel?.text = bioContent.bio
+      pagesCount += 1
     } else {
       self.bioView.isHidden = true
     }
@@ -91,6 +109,7 @@ class DiscoveryTableViewCell: UITableViewCell {
       self.doView.isHidden = false
       self.doView.writtenByLabel?.stylizeCreatorLabel(preText: "Written by ", boldText: doContent.creatorName)
       self.doView.contentLabel?.text = doContent.phrase
+      pagesCount += 1
     } else {
       self.doView.isHidden = true
     }
@@ -98,9 +117,59 @@ class DiscoveryTableViewCell: UITableViewCell {
       self.dontView.isHidden = false
       self.dontView.writtenByLabel?.stylizeCreatorLabel(preText: "Written by ", boldText: dontContent.creatorName)
       self.dontView.contentLabel?.text = dontContent.phrase
+      pagesCount += 1
     } else {
       self.dontView.isHidden = true
     }
+    
+    let indicatorSpacing: CGFloat = 8
+    let indicatorWidth: CGFloat = (self.contentScrollView.frame.width - (CGFloat(pagesCount + 1) * indicatorSpacing)) / CGFloat(pagesCount)
+    let totalItemWidth = indicatorSpacing + indicatorWidth
+    for index in 0..<pagesCount {
+      let indicatorView = UIView(frame: CGRect(x: indicatorSpacing + totalItemWidth * CGFloat(index) ,
+                                               y: self.contentScrollView.frame.height - 8,
+                                               width: indicatorWidth,
+                                               height: 4))
+      indicatorView.layer.cornerRadius = 2
+      self.indicatorViews.append(indicatorView)
+      self.itemsStackView.addSubview(indicatorView)
+    }
+    self.highlightIndicator(index: 0)
+  }
+  
+  func highlightIndicator(index: Int) {
+    let imageHighlightColor = UIColor(white: 1.0, alpha: 0.7)
+    let imageTintColor = UIColor(white: 1.0, alpha: 0.25)
+    let textHightlightColor = UIColor(white: 0.0, alpha: 0.3)
+    let textTintColor = UIColor(white: 0.0, alpha: 0.1)
+    var indexView: UIView?
+    var indexCount = 0
+    for contentView in self.contentStackView.arrangedSubviews where !contentView.isHidden {
+      if indexCount == index {
+        indexView = contentView
+        break
+      }
+      indexCount += 1
+    }
+    if let indexView = indexView {
+      
+      switch indexView {
+      case is UIImageView:
+        self.indicatorViews.forEach({ $0.backgroundColor = imageTintColor })
+        if index < self.indicatorViews.count {
+          self.indicatorViews[index].backgroundColor = imageHighlightColor
+        }
+      case is BioView, is DoDontView:
+        self.indicatorViews.forEach({ $0.backgroundColor = textTintColor })
+        if index < self.indicatorViews.count {
+          self.indicatorViews[index].backgroundColor = textHightlightColor
+        }
+      default:
+        break
+        
+      }
+    }
+    
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
@@ -114,7 +183,8 @@ class DiscoveryTableViewCell: UITableViewCell {
 extension DiscoveryTableViewCell: UIScrollViewDelegate {
   
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    
+    let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
+    self.highlightIndicator(index: Int(pageIndex))
   }
   
 }
@@ -137,6 +207,8 @@ extension DiscoveryTableViewCell {
     contentTextLabel.text = bioText
     contentTextLabel.stylizeDoDontLabel()
     contentTextLabel.translatesAutoresizingMaskIntoConstraints = false
+    contentTextLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    contentTextLabel.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
     containerView.addSubview(contentTextLabel)
     containerView.addConstraints([
       NSLayoutConstraint(item: contentTextLabel, attribute: .left, relatedBy: .equal,
@@ -154,8 +226,8 @@ extension DiscoveryTableViewCell {
     containerView.addConstraints([
       NSLayoutConstraint(item: writtenByLabel, attribute: .top, relatedBy: .equal,
                          toItem: contentTextLabel, attribute: .bottom, multiplier: 1.0, constant: 12.0),
-      NSLayoutConstraint(item: writtenByLabel, attribute: .bottom, relatedBy: .equal,
-                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -16.0)
+      NSLayoutConstraint(item: writtenByLabel, attribute: .bottom, relatedBy: .lessThanOrEqual,
+                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -20.0)
       
       ])
     
@@ -189,12 +261,43 @@ extension DiscoveryTableViewCell {
     let containerView = DoDontView()
     containerView.translatesAutoresizingMaskIntoConstraints = false
     
+    let iconImageView = UIImageView()
+    iconImageView.translatesAutoresizingMaskIntoConstraints = false
+    iconImageView.contentMode = .scaleAspectFit
+    containerView.addSubview(iconImageView)
+    
+    let titleLabel = UILabel()
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    titleLabel.stylizeProfileSectionTitleLabel()
+    containerView.addSubview(titleLabel)
+    
+    containerView.addConstraints([
+      NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal,
+                         toItem: iconImageView, attribute: .right, multiplier: 1.0, constant: 12),
+      NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal,
+                         toItem: containerView, attribute: .right, multiplier: 1.0, constant: -16),
+      NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal,
+                         toItem: containerView, attribute: .top, multiplier: 1.0, constant: 12),
+      NSLayoutConstraint(item: iconImageView, attribute: .left, relatedBy: .equal,
+                         toItem: containerView, attribute: .left, multiplier: 1.0, constant: 8),
+      NSLayoutConstraint(item: iconImageView, attribute: .width, relatedBy: .equal,
+                         toItem: iconImageView, attribute: .height, multiplier: 1.0, constant: 0),
+      NSLayoutConstraint(item: iconImageView, attribute: .width, relatedBy: .equal,
+                         toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 46),
+      NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal,
+                         toItem: titleLabel, attribute: .centerY, multiplier: 1.0, constant: 0)
+      ])
+    
     var fullContentText = ""
     switch type {
     case .doType:
-      fullContentText += "\"Do " + contentText + "\""
+      iconImageView.image = R.image.profileIconDo()
+      titleLabel.text = "Do"
+      fullContentText += contentText
     case .dontType:
-      fullContentText += "\"Don't " + contentText + "\""
+      iconImageView.image = R.image.profileIconDont()
+      titleLabel.text = "Don't"
+      fullContentText += contentText
     }
     
     let contentTextLabel = UILabel()
@@ -202,7 +305,8 @@ extension DiscoveryTableViewCell {
     contentTextLabel.text = fullContentText
     contentTextLabel.stylizeDoDontLabel()
     contentTextLabel.translatesAutoresizingMaskIntoConstraints = false
-    contentTextLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
+    contentTextLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    contentTextLabel.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
     containerView.addSubview(contentTextLabel)
     containerView.addConstraints([
       NSLayoutConstraint(item: contentTextLabel, attribute: .left, relatedBy: .equal,
@@ -210,19 +314,18 @@ extension DiscoveryTableViewCell {
       NSLayoutConstraint(item: contentTextLabel, attribute: .right, relatedBy: .equal,
                          toItem: containerView, attribute: .right, multiplier: 1.0, constant: -indentWidth),
       NSLayoutConstraint(item: contentTextLabel, attribute: .top, relatedBy: .equal,
-                         toItem: containerView, attribute: .top, multiplier: 1.0, constant: 0.0)
+                         toItem: titleLabel, attribute: .bottom, multiplier: 1.0, constant: 12.0)
       ])
     
     let writtenByLabel = UILabel()
     writtenByLabel.stylizeCreatorLabel(preText: "Written by ", boldText: creatorName)
     writtenByLabel.translatesAutoresizingMaskIntoConstraints = false
-    writtenByLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
     containerView.addSubview(writtenByLabel)
     containerView.addConstraints([
       NSLayoutConstraint(item: writtenByLabel, attribute: .top, relatedBy: .equal,
                          toItem: contentTextLabel, attribute: .bottom, multiplier: 1.0, constant: 12.0),
-      NSLayoutConstraint(item: writtenByLabel, attribute: .bottom, relatedBy: .equal,
-                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -16.0)
+      NSLayoutConstraint(item: writtenByLabel, attribute: .bottom, relatedBy: .lessThanOrEqual,
+                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -20.0)
       
       ])
     
