8909053b5a32c0880e650972a6e724236709a9dd
Avery Lamp <averylamp@gmail.com>
2019-04-02 02:53:07 -0400

    Updated tab images

--- Pear/Controllers/Me Tab/MeTabViewController.swift
+++ Pear/Controllers/Me Tab/MeTabViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class MeTabViewController: UIViewController {
   
-  var userProfiles: [FullProfileDisplayData] = []
-  
-  @IBOutlet weak var tableView: UITableView!
+  var fullProfile: FullProfileDisplayData?
+
   @IBOutlet weak var titleLabel: UILabel!
-  @IBOutlet weak var requestProfileButton: UIButton!
+  @IBOutlet weak var scrollView: UIScrollView!
+  
   class func instantiate() -> MeTabViewController? {
     let storyboard = UIStoryboard(name: String(describing: MeTabViewController.self), bundle: nil)
     guard let matchesVC = storyboard.instantiateInitialViewController() as? MeTabViewController else { return nil }
     return matchesVC
-  }
-  
-  @IBAction func requestProfileButtonClicked(_ sender: Any) {
-    
-    // swiftlint:disable:next line_length
-    self.alert(title: "Sorry 😢", message: "This feature is currently disabled in beta. If your friend also has beta access, have them make a profile for you and input your phone number.")
   }
   
 }
@@ -36,65 +30,50 @@ extension MeTabViewController {
     super.viewDidLoad()
     self.stylize()
     
-//    if let user = DataStore.shared.currentPearUser {
-//      for userProfile in user.userProfiles {
-//        if let fullProfile = try? FullProfileDisplayData(user: user, profiles: [userProfile]) {
-//          self.userProfiles.append(fullProfile)
-//        }
-//      }
-//    }
-    self.tableView.dataSource = self
-    self.tableView.delegate = self
+    if let user = DataStore.shared.currentPearUser {
+      self.fullProfile = FullProfileDisplayData(user: user)
+    }
+    
+    self.refreshFullStackVC()
+
   }
   
   override func viewDidAppear(_ animated: Bool) {
     
   }
   
+  func checkForUpdatedUser() {
+    
+  }
+  
   func stylize() {
-    self.requestProfileButton.stylizeDark()
+    
   }
   
-}
-
-// MARK: - UITableViewDelegate/DataSource
-extension MeTabViewController: UITableViewDelegate, UITableViewDataSource {
-  
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 120
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.userProfiles.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileMadeByTVC", for: indexPath) as? ProfileMadeByTableViewCell {
-      let profileData = self.userProfiles[indexPath.row]
-      cell.selectionStyle = .none
-      cell.profileFirstImageView.clipsToBounds = true
-      cell.profileFirstImageView.contentMode = .scaleAspectFill
-      cell.profileFirstImageView.image = nil
-      if let firstImageURL = profileData.imageContainers.first?.medium.imageURL, let imageURL = URL(string: firstImageURL) {
-        cell.profileFirstImageView.sd_setImage(with: imageURL, completed: nil)
-      }
-      cell.subtextLabel.stylizeSubtitleLabel()
-      if let originalCreatorName = profileData.originalCreatorName {
-        cell.subtextLabel.text = "Made by \(originalCreatorName)"
-      }
-      return cell
-    } else {
-      return UITableViewCell()
-    }
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let fullProfile = self.userProfiles[indexPath.row]
-    guard let fullProfileScrollVC = FullProfileScrollViewController.instantiate(fullProfileData: fullProfile) else {
-      print("Failed to create full profile Scroll View")
+  func refreshFullStackVC() {
+    self.scrollView.subviews.forEach({ $0.removeFromSuperview() })
+    
+    guard let fullProfile = self.fullProfile,
+      let fullProfileStackVC = FullProfileStackViewController.instantiate(userFullProfileData: fullProfile) else {
+      print("Failed to create full profiles stack VC")
       return
     }
-    self.navigationController?.pushViewController(fullProfileScrollVC, animated: true)
+    
+    self.addChild(fullProfileStackVC)
+    self.scrollView.addSubview(fullProfileStackVC.view)
+    fullProfileStackVC.view.translatesAutoresizingMaskIntoConstraints = false
+    
+    self.scrollView.addConstraints([
+      NSLayoutConstraint(item: fullProfileStackVC.view!, attribute: .centerX, relatedBy: .equal,
+                         toItem: self.scrollView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: fullProfileStackVC.view!, attribute: .width, relatedBy: .equal,
+                         toItem: self.scrollView, attribute: .width, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: fullProfileStackVC.view!, attribute: .top, relatedBy: .equal,
+                         toItem: self.scrollView, attribute: .top, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: fullProfileStackVC.view!, attribute: .bottom, relatedBy: .equal,
+                         toItem: self.scrollView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+      ])
+    fullProfileStackVC.didMove(toParent: self)
   }
   
 }
