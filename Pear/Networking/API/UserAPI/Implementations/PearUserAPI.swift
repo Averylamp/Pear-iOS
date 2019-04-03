67bef2bf88d29465be536c00f570b16d651293a0
Avery Lamp <averylamp@gmail.com>
2019-04-03 01:26:06 -0400

    Fixes birthday misalignment fixes #81

--- Pear/Networking/API/UserAPI/Implementations/PearUserAPI.swift
+++ Pear/Networking/API/UserAPI/Implementations/PearUserAPI.swift
@@ -90,8 +90,9 @@ extension PearUserAPI {
             do {
               let pearUser = try JSONDecoder().decode(PearUser.self, from: objectData)
               print("Successfully found Pear User")
-//              completion(.success(pearUser))
-              completion(.failure(UserAPIError.failedDeserialization))
+              completion(.success(pearUser))
+              // Uncomment this line to go through initial user setup
+//              completion(.failure(UserAPIError.failedDeserialization))
             } catch {
               print("Deserialization Error: \(error)")
               completion(.failure(UserAPIError.failedDeserialization))
