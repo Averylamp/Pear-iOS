71678bf751bb806ec3cb87960aecd7c1fbb01eef
Avery Lamp <averylamp@gmail.com>
2019-04-04 05:14:59 -0400

    Single request send

---
+++ Pear/Networking/API/MatchesAPI/MatchesAPI.swift
@@ -0,0 +1,24 @@
+//
+//  MatchesAPI.swift
+//  Pear
+//
+//  Created by Avery Lamp on 4/4/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+enum MatchesAPIError: Error {
+  case userNotLoggedIn
+  case failedDeserialization
+  case graphQLError(message: String)
+  case unknown
+  case unknownError(error: Error)
+}
+
+protocol MatchesAPI {
+  func createMatchRequest(sentByUserID: String,
+                          sentForUserID: String,
+                          receivedByUserID: String,
+                          requestText: String?,
+                          completion: @escaping(Result<Bool, MatchesAPIError>) -> Void)
+}
