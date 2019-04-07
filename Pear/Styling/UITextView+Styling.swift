d563c1fc2dbc6e384ef792f73825878ca29ffdc4
Avery Lamp <averylamp@gmail.com>
2019-04-07 04:22:21 -0400

    Expanding tv field

---
+++ Pear/Styling/UITextView+Styling.swift
@@ -0,0 +1,21 @@
+//
+//  UITextView+Styling.swift
+//  Pear
+//
+//  Created by Avery Lamp on 4/7/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextView {
+  
+  func stylizeEditTextLabel() {
+    if let font = R.font.nunitoRegular(size: 17) {
+      self.font = font
+    }
+    self.textColor = R.color.primaryTextColor()
+  }
+  
+}
