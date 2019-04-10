53ae802a35fa2f55dac1892ff2c62255d7c385e6
Joel Aguero <joelmaguero@gmail.com>
2019-04-09 21:22:03 -0400

    Update GenderInput logic so next view is ChoosePreferences

--- Pear/Controllers/Create Profile Flow/Gender Input/GetStartedChooseGenderViewController.swift
+++ Pear/Controllers/Create Profile Flow/Gender Input/GetStartedChooseGenderViewController.swift
@@ -56,9 +56,9 @@ class GetStartedChooseGenderViewController: UIViewController {
       break
     }
     
-    if let schoolVC = GetStartedSchoolViewController.instantiate(gettingStartedData: self.gettingStartedData) {
+    if let choosePreferencesVC = GetStartedChoosePreferencesViewController.instantiate(gettingStartedData: self.gettingStartedData) {
       Analytics.logEvent("finished_friend_gender", parameters: nil)
-      self.navigationController?.pushViewController(schoolVC, animated: true)
+      self.navigationController?.pushViewController(choosePreferencesVC, animated: true)
     } else {
       print("Failed to create School VC")
       return
