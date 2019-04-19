93708633d637cebd520f0812876dffa7c00acab1
Avery Lamp <averylamp@gmail.com>
2019-04-18 21:54:24 -0400

    Beginnings of user contact permissions page

--- Pear/Controllers/New Redesign/Create User/UserContactPermissionsViewController.swift
+++ Pear/Controllers/New Redesign/Create User/UserContactPermissionsViewController.swift
@@ -9,21 +9,50 @@
 import UIKit
 
 class UserContactPermissionsViewController: UIViewController {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var numberProfilesLabel: UILabel!
+  @IBOutlet weak var enableContactsButton: UIButton!
+  
+  /// Factory method for creating this view controller.
+  ///
+  /// - Returns: Returns an instance of this view controller.
+  class func instantiate() -> UserContactPermissionsViewController? {
+    guard let contactPermissionsVC = R.storyboard.userContactPermissionsViewController()
+      .instantiateInitialViewController() as? UserContactPermissionsViewController else { return nil }
+    return contactPermissionsVC
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Life Cycle
+extension UserContactPermissionsViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.stylize()
+  }
+  
+  func stylize() {
+    self.view.backgroundColor = R.color.backgroundColorBlue()
+    if let font = R.font.openSansExtraBold(size: 16) {
+      self.titleLabel.font = font
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    self.titleLabel.textColor = UIColor.white
+    if let font = R.font.openSansSemiBold(size: 16) {
+      self.numberProfilesLabel.font = font
     }
-    */
-
+    self.numberProfilesLabel.textColor = UIColor.white
+    self.numberProfilesLabel.text = "No one has peared you yet 😢.  Pear a friend!"
+    
+    if let font = R.font.openSansBold(size: 18) {
+      self.enableContactsButton.titleLabel?.font = font
+    }
+    self.enableContactsButton.setTitleColor(UIColor.black, for: .normal)
+    self.enableContactsButton.backgroundColor = R.color.backgroundColorYellow()
+    self.enableContactsButton.layer.cornerRadius = self.enableContactsButton.frame.height / 2.0
+    
+  }
+  
 }
