64c08bb4b89446c97acd80e79f8dea4bfbb61127
Avery Lamp <averylamp@gmail.com>
2019-04-21 14:01:14 -0400

    Contact list added

--- Pear/Controllers/New Redesign/Create User/UserContactListViewController.swift
+++ Pear/Controllers/New Redesign/Create User/UserContactListViewController.swift
@@ -7,23 +7,129 @@
 //
 
 import UIKit
+import Contacts
+
+struct ContactListItem {
+  let contact: CNContact
+  let phoneNumber: String
+  let name: String
+  var enabled: Bool = true
+}
 
 class UserContactListViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchContainerView: UIView!
+  @IBOutlet weak var searchInputTextField: UITextField!
+  
+  var allContacts: [ContactListItem] = []
+  var filteredContacts: [ContactListItem] = []
+  /// Factory method for creating this view controller.
+  ///
+  /// - Returns: Returns an instance of this view controller.
+  class func instantiate(contacts: [CNContact]) -> UserContactListViewController? {
+    guard let contactListVC = R.storyboard.userContactListViewController()
+      .instantiateInitialViewController() as? UserContactListViewController else { return nil }
+    contacts.forEach({
+      guard let phone = $0.phoneNumbers.first?.value.stringValue.filter("0123456789".contains) else {
+        return
+      }
+      var fullName = ""
+      fullName += $0.givenName
+      if $0.familyName.count > 0, let initial = $0.familyName.firstUppercased.first {
+        fullName += " " + String(initial)  + "."
+      }
+      if fullName.count > 3 && phone.count == 10 {
+        let item = ContactListItem(contact: $0, phoneNumber: phone, name: fullName, enabled: true)
+        contactListVC.allContacts.append(item)
+      }
+    })
+    contactListVC.allContacts.sort { (cl1, cl2) -> Bool in
+      return cl1.name < cl2.name
     }
+    contactListVC.filteredContacts.append(contentsOf: contactListVC.allContacts)
+    return contactListVC
+  }
+  
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Life Cycle
+extension UserContactListViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.stylize()
+    self.setup()
+    self.fetchExistingUsers()
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  func stylize() {
+    self.view.backgroundColor = R.color.backgroundColorYellow()
+    self.tableView.backgroundColor = nil
+    self.tableView.separatorStyle = .none
+    self.searchContainerView.layer.cornerRadius = 8
+    if let font = R.font.openSansExtraBold(size: 24) {
+      self.searchInputTextField.font = font
     }
-    */
+  }
+  
+  func setup() {
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    self.searchInputTextField.delegate = self
+  }
+  
+  func fetchExistingUsers() {
+    
+  }
+  
+}
 
+// MARK: - UITableViewDelegate/DataSource
+extension UserContactListViewController: UITableViewDelegate, UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.filteredContacts.count
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return UITableView.automaticDimension
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.contactLIstItemTVC.identifier, for: indexPath) as? ContactListItemTableViewCell else {
+      print("Failed to create contact List item")
+      return UITableViewCell()
+    }
+    let contactItem = self.filteredContacts[indexPath.row]
+    cell.configure(contactItem: contactItem)
+    
+    return cell
+  }
+  
+}
+
+// MARK: - UITextFieldDelegate
+extension UserContactListViewController: UITextFieldDelegate {
+  
+  func updateSearch() {
+    guard let text = self.searchInputTextField.text else {
+      self.filteredContacts = self.allContacts
+      return
+    }
+    
+    if text.count > 0 {
+      
+    } else {
+      self.filteredContacts = self.allContacts
+    }
+    
+  }
+  
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    return true
+  }
+  
 }
