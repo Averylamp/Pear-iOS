1f16eaa6facb7160dbdbe0718ac6ee983a33f4dc
Lorenzo Bernaschina <lorenzo.bernaschina@mail.polimi.it>
2019-04-24 04:08:21 -0400

    New layout

--- Pear/Controllers/Discovery Flow/Simple Discovery/DiscoverySimpleViewController.swift
+++ Pear/Controllers/Discovery Flow/Simple Discovery/DiscoverySimpleViewController.swift
@@ -261,16 +261,6 @@ extension DiscoverySimpleViewController: UITableViewDelegate, UITableViewDataSou
     cell.delegate = self
     let fullProfile = self.fullProfiles[indexPath.row]
     cell.selectionStyle = .none
-    cell.configureCell(profileData: fullProfile)
-    cell.cardView.layer.cornerRadius = 12
-    cell.cardView.layer.borderColor = UIColor(white: 0.95, alpha: 1.0).cgColor
-    cell.cardView.layer.borderWidth = 1
-    cell.cardView.clipsToBounds = true
-    cell.cardShadowView.layer.shadowOpacity = 0.4
-    cell.cardShadowView.layer.shadowColor = R.color.shadowColor()?.cgColor
-    cell.cardShadowView.layer.shadowOffset = CGSize(width: 1, height: 1 )
-    cell.cardShadowView.layer.shadowRadius = 2
-    cell.cardShadowView.layer.cornerRadius = cell.cardView.layer.cornerRadius
     return cell
   }
   
@@ -301,8 +291,8 @@ extension DiscoverySimpleViewController: DiscoveryTableViewCellDelegate {
     
   }
   
-  func fullProfileViewTriggered(profileData: FullProfileDisplayData) {
-    self.presentFullProfile(fullProfile: profileData)
-  }
+//  func fullProfileViewTriggered(profileData: FullProfileDisplayData) {
+//    self.presentFullProfile(fullProfile: profileData)
+//  }
   
 }
