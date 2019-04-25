5e966a76127b327112df358301aea3ad1961e351
Avery Lamp <averylamp@gmail.com>
2019-04-25 06:01:25 -0400

    New discovery feed styling

--- Pear/Controllers/Profiles/Profile Boast Roast/ProfileBoastRoastViewController.swift
+++ Pear/Controllers/Profiles/Profile Boast Roast/ProfileBoastRoastViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
+enum ItemStyle {
+  case discovery
+  case profile
+}
+
 class ProfileBoastRoastViewController: UIViewController {
 
   var boastRoastItem: BoastRoastItem!
   var userName: String?
+  var maxLines: Int!
+  var itemStyle: ItemStyle!
   
   @IBOutlet weak var iconImageView: UIImageView!
   @IBOutlet weak var contentLabel: UILabel!
@@ -20,11 +27,14 @@ class ProfileBoastRoastViewController: UIViewController {
   /// Factory method for creating this view controller.
   ///
   /// - Returns: Returns an instance of this view controller.
-  class func instantiate(boastRoastItem: BoastRoastItem, userName: String?) -> ProfileBoastRoastViewController? {
+  class func instantiate(boastRoastItem: BoastRoastItem, userName: String?,
+                         style: ItemStyle, maxLines: Int = 0) -> ProfileBoastRoastViewController? {
     guard let profileBoastRoastVC = R.storyboard.profileBoastRoastViewController()
       .instantiateInitialViewController() as? ProfileBoastRoastViewController else { return nil }
     profileBoastRoastVC.boastRoastItem = boastRoastItem
     profileBoastRoastVC.userName = userName
+    profileBoastRoastVC.itemStyle = style
+    profileBoastRoastVC.maxLines = maxLines
     return profileBoastRoastVC
   }
   
@@ -46,7 +56,28 @@ extension ProfileBoastRoastViewController {
       self.iconImageView.image = R.image.iconRoast()
       self.titleLabel.text = "\(boastRoastItem.authorFirstName) roasted \(self.userName ?? "")"
     }
+    self.contentLabel.numberOfLines = self.maxLines
     self.contentLabel.text = boastRoastItem.content
+    switch self.itemStyle! {
+    case .discovery:
+      if let font = R.font.openSansBold(size: 12) {
+        self.titleLabel.font = font
+      }
+      if let font = R.font.openSansRegular(size: 12) {
+        self.contentLabel.font = font
+      }
+      self.titleLabel.textColor = UIColor.black
+      self.contentLabel.textColor = UIColor.black
+    case .profile:
+      if let font = R.font.openSansBold(size: 16) {
+        self.titleLabel.font = font
+      }
+      if let font = R.font.openSansBold(size: 16) {
+        self.contentLabel.font = font
+      }
+      self.titleLabel.textColor = UIColor.black
+      self.contentLabel.textColor = UIColor(white: 0.0, alpha: 0.5)
+    }
   }
   
 }
