74442912be2c13d23810d4cad04f28374fc0bc4d
Avery Lamp <averylamp@gmail.com>
2019-05-02 20:14:54 -0400

    Added log out and delete buttons

--- Pear/Controllers/Me Tab/Main Full Screen Display/MeTabViewController.swift
+++ Pear/Controllers/Me Tab/Main Full Screen Display/MeTabViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import MessageUI
 
 extension Notification.Name {
   static let refreshMeTab = Notification.Name("refreshMeTab")
@@ -15,12 +17,12 @@ extension Notification.Name {
 class MeTabViewController: UIViewController {
   
   var fullProfile: FullProfileDisplayData?
-
+  
   @IBOutlet weak var floatingEditButton: UIButton!
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var scrollView: UIScrollView!
   var currentPearUser: PearUser?
-  
+  var isDeleting: Bool = false
   class func instantiate() -> MeTabViewController? {
     let storyboard = UIStoryboard(name: String(describing: MeTabViewController.self), bundle: nil)
     guard let matchesVC = storyboard.instantiateInitialViewController() as? MeTabViewController else { return nil }
@@ -114,7 +116,7 @@ extension MeTabViewController {
     
     if pearUser.endorserIDs.count == 0,
       let incompleteImage = R.image.meIncompleteProfile() {
-//      fullProfile.rawImages.append(incompleteImage)
+      //      fullProfile.rawImages.append(incompleteImage)
     }
     guard let fullProfileStackVC = FullProfileStackViewController.instantiate(userFullProfileData: fullProfile) else {
       print("Failed to create full profiles stack VC")
@@ -137,32 +139,147 @@ extension MeTabViewController {
       ])
     fullProfileStackVC.didMove(toParent: self)
     
-    let containerView = UIView()
-    containerView.translatesAutoresizingMaskIntoConstraints = false
-    let inviteButton = UIButton()
-    inviteButton.addTarget(self, action: #selector(MeTabViewController.inviteFriendsButtonClicked(sender:)), for: .touchUpInside)
-    inviteButton.translatesAutoresizingMaskIntoConstraints = false
-    inviteButton.setTitle("Invite Friends", for: .normal)
-    containerView.addSubview(inviteButton)
-    inviteButton.addConstraint(NSLayoutConstraint(item: inviteButton, attribute: .height, relatedBy: .equal,
-                                                  toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50))
+    let inviteButtonView = self.getInviteButton()
+    fullProfileStackVC.stackView.addArrangedSubview(inviteButtonView)
     
-    containerView.addConstraints([
-      NSLayoutConstraint(item: inviteButton, attribute: .left, relatedBy: .equal,
-                         toItem: containerView, attribute: .left, multiplier: 1.0, constant: 20.0),
-      NSLayoutConstraint(item: inviteButton, attribute: .right, relatedBy: .equal,
-                         toItem: containerView, attribute: .right, multiplier: 1.0, constant: -20.0),
-      NSLayoutConstraint(item: inviteButton, attribute: .top, relatedBy: .equal,
-                         toItem: containerView, attribute: .top, multiplier: 1.0, constant: 8.0),
-      NSLayoutConstraint(item: inviteButton, attribute: .bottom, relatedBy: .equal,
-                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -8.0)
-      ])
-
-    fullProfileStackVC.stackView.addArrangedSubview(containerView)
+    let logOutButtonView = self.logoutButton()
+    fullProfileStackVC.stackView.addArrangedSubview(logOutButtonView)
+    
+    let deleteProfileButtonView = self.deleteProfileButton()
+    fullProfileStackVC.stackView.addArrangedSubview(deleteProfileButtonView)
     
     self.view.layoutIfNeeded()
+  }
+  
+  @objc func logoutButtonClicked() {
+    let defaultsName = Bundle.main.bundleIdentifier!
+    UserDefaults.standard.removePersistentDomain(forName: defaultsName)
+    do {
+      try Auth.auth().signOut()
+    } catch {
+      print("Failed Firebase Auth")
+    }
+    DispatchQueue.main.async {
+      guard let loadingScreen = LoadingScreenViewController.instantiate() else {
+        print("Failed to instantiate loadings screen")
+        return
+      }
+      self.navigationController?.setViewControllers([loadingScreen], animated: true)
+    }
+  }
+  
+  @objc func deleleProfileButtonClicked() {
+    guard !self.isDeleting else {
+      return
+    }
+    self.isDeleting = true
+    DispatchQueue.main.async {
+      let alertController =
+        UIAlertController(title: "Are you sure?",
+                          message: "Sorry to see you go.  We currently don't have a streamlined deletion experience, but will handle your request manually within 48 hours",
+                          preferredStyle: .alert)
+      
+      let cancelAlert = UIAlertAction(title: "Cancel", style: .default, handler: { (_) in
+        self.isDeleting = false
+      })
+      let deleteAlert = UIAlertAction(title: "Continue with Deletion", style: .destructive) { (_) in
+        DispatchQueue.main.async {
+          let mailComposer = MFMailComposeViewController()
+          mailComposer.setSubject("[DELETE] Delete my profile please!")
+          mailComposer.setToRecipients(["support@getpear.com"])
+          let messageBody = """
+          Name: \(DataStore.shared.currentPearUser?.firstName ?? "") \(DataStore.shared.currentPearUser?.lastName ?? "")
+          Phone Number: \(DataStore.shared.currentPearUser?.phoneNumber ?? "")
+          
+          If possible, we'd also love to know why you want to delete your profile and any feedback you may have for us.
+          Feel free to fill out the following
+          Reason:
+          
+          Feedback:
+          
+          """
+          mailComposer.setMessageBody(messageBody, isHTML: false)
+          mailComposer.mailComposeDelegate = self
+          self.present(mailComposer, animated: true, completion: nil)
+        }
+      }
+      alertController.addAction(cancelAlert)
+      alertController.addAction(deleteAlert)
+      self.present(alertController, animated: true, completion: nil)
+    }
+  }
+  
+  func deleteProfileButton() -> UIView {
+    let (containerView, button) = self.getButton()
+    button.setTitle("Delete Profile", for: .normal)
+    button.addTarget(self, action: #selector(MeTabViewController.deleleProfileButtonClicked), for: .touchUpInside)
+    button.backgroundColor = R.color.backgroundColorRed()
+    button.addButtonShadow()
+    return containerView
+  }
+  
+  func logoutButton() -> UIView {
+    let (containerView, button) = self.getButton()
+    button.setTitle("Log Out", for: .normal)
+    button.addTarget(self, action: #selector(MeTabViewController.logoutButtonClicked), for: .touchUpInside)
+    button.backgroundColor = R.color.backgroundColorOrange()
+    button.addButtonShadow()
+    return containerView
+  }
+  
+  func getInviteButton() -> UIView {
+    let (containerView, button) = self.getButton()
+    button.setTitle("Invite Friends", for: .normal)
+    button.addTarget(self, action: #selector(MeTabViewController.inviteFriendsButtonClicked(sender:)), for: .touchUpInside)
+    containerView.layoutIfNeeded()
+    button.stylizeDark()
+    return containerView
+  }
+  
+  func getButton() -> (container: UIView, button: UIButton) {
+    let containerView = UIView()
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    containerView.addSubview(button)
+    button.addConstraint(NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal,
+                                            toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50))
+    button.layer.cornerRadius = 25
     
-    inviteButton.stylizeDark()
+    containerView.addConstraints([
+      NSLayoutConstraint(item: button, attribute: .left, relatedBy: .equal,
+                         toItem: containerView, attribute: .left, multiplier: 1.0, constant: 20.0),
+      NSLayoutConstraint(item: button, attribute: .right, relatedBy: .equal,
+                         toItem: containerView, attribute: .right, multiplier: 1.0, constant: -20.0),
+      NSLayoutConstraint(item: button, attribute: .top, relatedBy: .equal,
+                         toItem: containerView, attribute: .top, multiplier: 1.0, constant: 8.0),
+      NSLayoutConstraint(item: button, attribute: .bottom, relatedBy: .equal,
+                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -8.0)
+      ])
+    return (container: containerView, button: button)
+  }
+  
+}
+
+// MARK: - MFMailComposerDelegate
+extension MeTabViewController: MFMailComposeViewControllerDelegate {
+  
+  func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    switch result {
+    case .sent:
+      DispatchQueue.main.async {
+        controller.dismiss(animated: true, completion: nil)
+      }
+      SentryHelper
+        .generateSentryMessageEvent(level: .fatal,
+                                    message: "Delete User: \(DataStore.shared.currentPearUser?.documentID ?? ""), Phone: \(DataStore.shared.currentPearUser?.phoneNumber ?? "")")
+      self.logoutButtonClicked()
+    default :
+      DispatchQueue.main.async {
+        controller.dismiss(animated: true, completion: nil)
+      }
+    }
+    self.isDeleting = false
   }
   
 }
