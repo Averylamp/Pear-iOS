a1771d8d96f7ed92f3b19167fcc0cffefed2cb73
Avery Lamp <averylamp@gmail.com>
2019-05-19 08:29:51 -0400

    Deserializes Matching Demogrpahics

--- Pear/Models/GraphQL Models/User/MatchingDemographics.swift
+++ Pear/Models/GraphQL Models/User/MatchingDemographics.swift
@@ -12,11 +12,19 @@ enum MatchingDemographicsKeys: String, CodingKey {
   case gender
   case age
   case location
+  case ethnicity
+  case educationLevel
+  case religion
+  case politicalView
+  case drinking
+  case smoking
+  case cannabis
+  case drugs
 }
 
 class MatchingDemographics: Decodable, Equatable, GraphQLDecodable {
   static func graphQLAllFields() -> String {
-   return "{ gender age location \(LocationObject.graphQLAllFields()) }"
+   return "{ gender age location \(LocationObject.graphQLAllFields()) ethnicity \(DemographicsItem<EthnicityEnum>.graphQLAllFields()) educationLevel \(DemographicsItem<EducationLevelEnum>.graphQLAllFields()) religion \(DemographicsItem<ReligionEnum>.graphQLAllFields()) politicalView \(DemographicsItem<PoliticsEnum>.graphQLAllFields()) drinking \(DemographicsItem<HabitsEnum>.graphQLAllFields()) smoking \(DemographicsItem<HabitsEnum>.graphQLAllFields()) cannabis \(DemographicsItem<HabitsEnum>.graphQLAllFields()) drugs \(DemographicsItem<HabitsEnum>.graphQLAllFields()) }"
   }
   
   static func == (lhs: MatchingDemographics, rhs: MatchingDemographics) -> Bool {
@@ -28,6 +36,14 @@ class MatchingDemographics: Decodable, Equatable, GraphQLDecodable {
   var gender: GenderEnum?
   var age: Int?
   var location: LocationObject?
+  var ethnicity: DemographicsItem<EthnicityEnum>
+  var educationLevel: DemographicsItem<EducationLevelEnum>
+  var religion: DemographicsItem<ReligionEnum>
+  var politicalView: DemographicsItem<PoliticsEnum>
+  var drinking: DemographicsItem<HabitsEnum>
+  var smoking: DemographicsItem<HabitsEnum>
+  var cannabis: DemographicsItem<HabitsEnum>
+  var drugs: DemographicsItem<HabitsEnum>
   
   required init(from decoder: Decoder) throws {
     let values = try decoder.container(keyedBy: MatchingDemographicsKeys.self)
@@ -41,5 +57,13 @@ class MatchingDemographics: Decodable, Equatable, GraphQLDecodable {
     if let location = try? values.decode(LocationObject.self, forKey: .location) {
       self.location = location
     }
+    self.ethnicity = try values.decode(DemographicsItem<EthnicityEnum>.self, forKey: .ethnicity)
+    self.educationLevel = try values.decode(DemographicsItem<EducationLevelEnum>.self, forKey: .educationLevel)
+    self.religion = try values.decode(DemographicsItem<ReligionEnum>.self, forKey: .religion)
+    self.politicalView = try values.decode(DemographicsItem<PoliticsEnum>.self, forKey: .politicalView)
+    self.drinking = try values.decode(DemographicsItem<HabitsEnum>.self, forKey: .drinking)
+    self.smoking = try values.decode(DemographicsItem<HabitsEnum>.self, forKey: .smoking)
+    self.cannabis = try values.decode(DemographicsItem<HabitsEnum>.self, forKey: .cannabis)
+    self.drugs = try values.decode(DemographicsItem<HabitsEnum>.self, forKey: .drugs)    
   }
 }
