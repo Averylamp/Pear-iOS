90c194b838e9ac74256bd3a7de8ab14e42370d80
briangu33 <briangu33@gmail.com>
2019-05-19 13:39:16 -0400

    message composer and contact picker

--- Pear/Controllers/New New Redesign/Onboarding/Join With Friend/Friend Info/OnboardingFriendInfoViewController.swift
+++ Pear/Controllers/New New Redesign/Onboarding/Join With Friend/Friend Info/OnboardingFriendInfoViewController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
+import MessageUI
 import FirebaseAnalytics
+import ContactsUI
 
 class OnboardingFriendInfoViewController: UIViewController {
 
@@ -18,12 +21,19 @@ class OnboardingFriendInfoViewController: UIViewController {
   
   @IBOutlet weak var progressBarWidthConstraint: NSLayoutConstraint!
   
+  var friendFirstName: String!
+  var friendGender: GenderEnum!
+  var profileData: ProfileCreationData?
+  var activityIndicator = NVActivityIndicatorView(frame: CGRect.zero)
+  
   /// Factory method for creating this view controller.
   ///
   /// - Returns: Returns an instance of this view controller.
-  class func instantiate() -> OnboardingFriendInfoViewController? {
+  class func instantiate(friendFirstName: String, friendGender: GenderEnum) -> OnboardingFriendInfoViewController? {
     guard let onboardingFriendInfoVC = R.storyboard.onboardingFriendInfoViewController()
       .instantiateInitialViewController() as? OnboardingFriendInfoViewController else { return nil }
+    onboardingFriendInfoVC.friendFirstName = friendFirstName
+    onboardingFriendInfoVC.friendGender = friendGender
     return onboardingFriendInfoVC
   }
   
@@ -33,6 +43,30 @@ class OnboardingFriendInfoViewController: UIViewController {
   
   @IBAction func continueButtonClicked(_ sender: Any) {
     HapticFeedbackGenerator.generateHapticFeedbackImpact(style: .light)
+    self.promptContactsPicker()
+  }
+  
+  func promptMessageComposer() {
+    print("****prompting message composer****")
+    guard let profileData = self.profileData else {
+      print("profile data not found")
+      return
+    }
+    guard let messageVC = self.getMessageComposer(profileData: profileData) else {
+      print("Could not create Message VC")
+      Analytics.logEvent("CP_sendProfileSMS_FAIL", parameters: nil)
+      self.createDetachedProfile(profileData: profileData,
+                                 completion: self.createDetachedProfileCompletion(result:))
+      return
+    }
+    messageVC.messageComposeDelegate = self
+    // #if DEVMODE
+    // self.createDetachedProfile(profileData: profileData,
+    //                            completion: self.createDetachedProfileCompletion(result:))
+    // return
+    // #endif
+    self.present(messageVC, animated: true, completion: nil)
+    Analytics.logEvent("CP_sendProfileSMS_START", parameters: nil)
   }
   
 }
@@ -85,5 +119,113 @@ extension OnboardingFriendInfoViewController: ProgressBarProtocol {
   
   var progressBarTotalPages: Int {
     return 2
+  }
+}
+
+// MARK: - PromptSMSProtocol
+extension OnboardingFriendInfoViewController: PromptSMSProtocol {
+  
+}
+
+extension OnboardingFriendInfoViewController: ProfileCreationProtocol, CNContactPickerDelegate {
+  
+  func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+    self.didSelectContact(contact: contact)
+    picker.dismiss(animated: true)
+  }
+  
+  func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
+    self.didSelectContactProperty(contactProperty: contactProperty)
+    picker.dismiss(animated: true)
+  }
+  
+  func receivedProfileCreationData(creationData: ProfileCreationData) {
+    self.profileData = creationData
+    self.profileData?.firstName = self.friendFirstName
+    self.profileData?.gender = self.friendGender
+    DispatchQueue.main.async {
+      self.promptMessageComposer()
+    }
+  }
+  
+  func recievedProfileCreationError(title: String, message: String?) {
+    DispatchQueue.main.async {
+      let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      self.present(alert, animated: true)
+    }
+  }
+  
+  func promptContactsPicker() {
+    let cnPicker = self.getContactsPicker()
+    cnPicker.delegate = self
+    
+    self.continueButton.isEnabled = false
+    self.activityIndicator = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40),
+                                                     type: NVActivityIndicatorType.lineScalePulseOut,
+                                                     color: StylingConfig.textFontColor,
+                                                     padding: 0)
+    self.view.addSubview(activityIndicator)
+    activityIndicator.center = CGPoint(x: self.view.center.x,
+                                       y: self.continueButton.frame.origin.y +
+                                        self.continueButton.frame.height + 40)
+    activityIndicator.startAnimating()
+    self.present(cnPicker, animated: true, completion: nil)
+  }
+  
+}
+
+// MARK: - MFMessageComposeViewControllerDelegate
+extension OnboardingFriendInfoViewController: MFMessageComposeViewControllerDelegate {
+  func createDetachedProfileCompletion(result: Result<PearDetachedProfile, (errorTitle: String, errorMessage: String)?>) {
+    switch result {
+    case .success:
+      DispatchQueue.main.async {
+        guard let basicInfoVC = OnboardingBasicInfoViewController.instantiate() else {
+          print("Failed to create basic info VC")
+          return
+        }
+        self.navigationController?.setViewControllers([basicInfoVC], animated: true)
+      }
+    case .failure(let error):
+      if let error = error {
+        DispatchQueue.main.async {
+          self.alert(title: error.errorTitle, message: error.errorMessage)
+        }
+      }
+    }
+    DispatchQueue.main.async {
+      self.activityIndicator.stopAnimating()
+      self.continueButton.isEnabled = true
+    }
+  }
+  
+  func dismissMessageVC(controller: MFMessageComposeViewController) {
+    controller.dismiss(animated: true) {
+      DispatchQueue.main.async {
+        self.continueButton.isEnabled = true
+        self.activityIndicator.stopAnimating()
+      }
+    }
+  }
+  
+  func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+    switch result {
+    case .cancelled, .failed:
+      self.dismissMessageVC(controller: controller)
+    case .sent:
+      controller.dismiss(animated: true) {
+        if let profileData = self.profileData {
+          DispatchQueue.main.async {
+            self.createDetachedProfile(profileData: profileData,
+                                       completion: self.createDetachedProfileCompletion(result:))
+          }
+        } else {
+          print("couldn't find profile data")
+        }
+      }
+    @unknown default:
+      fatalError()
+    }
   }
 }
