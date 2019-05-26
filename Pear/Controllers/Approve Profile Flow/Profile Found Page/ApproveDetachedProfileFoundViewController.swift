f847a570c8d5e2bfb389048139c9698722ef4929
Avery Lamp <averylamp@gmail.com>
2019-05-26 09:15:52 -0400

    nits and adds prompts to profile approval

--- Pear/Controllers/Approve Profile Flow/Profile Found Page/ApproveDetachedProfileFoundViewController.swift
+++ Pear/Controllers/Approve Profile Flow/Profile Found Page/ApproveDetachedProfileFoundViewController.swift
@@ -14,6 +14,7 @@ class ApproveDetachedProfileFoundViewController: UIViewController {
 
   var detachedProfile: PearDetachedProfile!
   
+  @IBOutlet weak var stackView: UIStackView!
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var nextButton: UIButton!
   @IBOutlet weak var friendImage: UIImageView!
@@ -79,7 +80,12 @@ extension ApproveDetachedProfileFoundViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.setup()
     self.stylize()
+  }
+  
+  func setup() {
+    detachedProfile.questionResponses.forEach({self.addAnsweredPrompt(response: $0)})
   }
   
   func stylize() {
@@ -113,4 +119,89 @@ extension ApproveDetachedProfileFoundViewController {
       self.friendImage.isHidden = true
     }
   }
+}
+
+//MARK: - Prompt Generation
+extension ApproveDetachedProfileFoundViewController {
+  
+  func addAnsweredPrompt(response: QuestionResponseItem) {
+    let containerView = UIView()
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    let cardView = UIView()
+    cardView.isUserInteractionEnabled = false
+    cardView.translatesAutoresizingMaskIntoConstraints = false
+    containerView.addSubview(cardView)
+    cardView.layer.cornerRadius = 12
+    cardView.layer.borderWidth = 2.0
+    cardView.layer.borderColor = UIColor(white: 0.95, alpha: 1.0).cgColor
+    
+    containerView.addConstraints([
+      NSLayoutConstraint(item: cardView, attribute: .left, relatedBy: .equal,
+                         toItem: containerView, attribute: .left, multiplier: 1.0, constant: 20.0),
+      NSLayoutConstraint(item: cardView, attribute: .right, relatedBy: .equal,
+                         toItem: containerView, attribute: .right, multiplier: 1.0, constant: -20.0),
+      NSLayoutConstraint(item: cardView, attribute: .top, relatedBy: .equal,
+                         toItem: containerView, attribute: .top, multiplier: 1.0, constant: 6.0),
+      NSLayoutConstraint(item: cardView, attribute: .bottom, relatedBy: .equal,
+                         toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: -6.0)
+      ])
+    
+    let promptLabel = UILabel()
+    promptLabel.translatesAutoresizingMaskIntoConstraints = false
+    if let font = R.font.openSansBold(size: 14.0) {
+      promptLabel.font = font
+    }
+    promptLabel.text = response.question.questionText
+    promptLabel.textColor = R.color.primaryTextColor()
+    promptLabel.numberOfLines = 0
+    cardView.addSubview(promptLabel)
+    cardView.addConstraints([
+      NSLayoutConstraint(item: promptLabel, attribute: .top, relatedBy: .equal,
+                         toItem: cardView, attribute: .top, multiplier: 1.0, constant: 12.0),
+      NSLayoutConstraint(item: promptLabel, attribute: .left, relatedBy: .equal,
+                         toItem: cardView, attribute: .left, multiplier: 1.0, constant: 12.0)
+      
+      ])
+    
+    let responseLabel = UILabel()
+    responseLabel.translatesAutoresizingMaskIntoConstraints = false
+    if let font = R.font.openSansSemiBold(size: 14) {
+      responseLabel.font = font
+    }
+    responseLabel.text = response.responseBody
+    responseLabel.textColor = R.color.secondaryTextColor()
+    responseLabel.numberOfLines = 0
+    cardView.addSubview(responseLabel)
+    cardView.addConstraints([
+      NSLayoutConstraint(item: responseLabel, attribute: .top, relatedBy: .equal,
+                         toItem: promptLabel, attribute: .bottom, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: responseLabel, attribute: .left, relatedBy: .equal,
+                         toItem: cardView, attribute: .left, multiplier: 1.0, constant: 12.0),
+      NSLayoutConstraint(item: responseLabel, attribute: .bottom, relatedBy: .equal,
+                         toItem: cardView, attribute: .bottom, multiplier: 1.0, constant: -12.0)
+      ])
+    
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    imageView.image = R.image.updateUserIconMoreIcon()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    cardView.addSubview(imageView)
+    cardView.addConstraints([
+      NSLayoutConstraint(item: promptLabel, attribute: .right, relatedBy: .equal,
+                         toItem: imageView, attribute: .left, multiplier: 1.0, constant: -4.0),
+      NSLayoutConstraint(item: responseLabel, attribute: .right, relatedBy: .equal,
+                         toItem: imageView, attribute: .left, multiplier: 1.0, constant: -4.0),
+      NSLayoutConstraint(item: imageView, attribute: .right, relatedBy: .equal,
+                         toItem: cardView, attribute: .right, multiplier: 1.0, constant: -12.0),
+      NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal,
+                         toItem: cardView, attribute: .centerY, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal,
+                         toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 26.0),
+      NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal,
+                         toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 26.0)
+      ])
+    
+    self.stackView.addArrangedSubview(containerView)
+  }
+
 }
