200ac86571ca5ca57e8532a5b91ca0bd48d8e51b
briangu33 <briangu33@gmail.com>
2019-05-30 15:38:42 -0400

    fix iphone SE qr code scanner

--- Pear/Controllers/Discovery Flow/QR Scanner/QRCodeScannerViewController.swift
+++ Pear/Controllers/Discovery Flow/QR Scanner/QRCodeScannerViewController.swift
@@ -66,7 +66,8 @@ extension QRCodeScannerViewController {
   
   func setup() {
     // Get the back-facing camera for capturing videos
-    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTelephotoCamera],
+                                                                  mediaType: AVMediaType.video, position: .back)
     
     guard let captureDevice = deviceDiscoverySession.devices.first else {
       print("Failed to get the camera device")
