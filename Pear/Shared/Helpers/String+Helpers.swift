e1ccc333d9f2728530f7e0e1ab23486838eb46a9
Avery Lamp <averylamp@gmail.com>
2019-06-18 21:37:52 -0400

    Adds referal codes

--- Pear/Shared/Helpers/String+Helpers.swift
+++ Pear/Shared/Helpers/String+Helpers.swift
@@ -143,4 +143,34 @@ extension StringProtocol {
   var firstCapitalized: String {
     return prefix(1).capitalized + dropFirst()
   }
+  
+  func index(of string: Self, options: String.CompareOptions = []) -> Index? {
+    return range(of: string, options: options)?.lowerBound
+  }
+  func endIndex(of string: Self, options: String.CompareOptions = []) -> Index? {
+    return range(of: string, options: options)?.upperBound
+  }
+  func indexes(of string: Self, options: String.CompareOptions = []) -> [Index] {
+    var result: [Index] = []
+    var startIndex = self.startIndex
+    while startIndex < endIndex,
+      let range = self[startIndex...].range(of: string, options: options) {
+        result.append(range.lowerBound)
+        startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+          index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+    }
+    return result
+  }
+  func ranges(of string: Self, options: String.CompareOptions = []) -> [Range<Index>] {
+    var result: [Range<Index>] = []
+    var startIndex = self.startIndex
+    while startIndex < endIndex,
+      let range = self[startIndex...].range(of: string, options: options) {
+        result.append(range)
+        startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+          index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+    }
+    return result
+  }
+  
 }
