d4fb8cf8851f2a925f9c1831c77a1627f3a00196
Avery Lamp <averylamp@gmail.com>
2019-06-18 22:54:56 -0400

    Cleanup

--- Pear/Controllers/Discovery Flow/Discovery Full Profile View/DiscoveryFullProfileViewController+Analytics.swift
+++ Pear/Controllers/Discovery Flow/Discovery Full Profile View/DiscoveryFullProfileViewController+Analytics.swift
@@ -12,10 +12,8 @@ import UIKit
 extension DiscoveryFullProfileViewController {
   
   func slackHelperDetails() -> String {
-    print("Max content offset: \(self.maxContentOffset), frameHeight: \(self.view.frame.height), contentHeight: \(self.scrollView.contentSize.height)")
     let percentProfileSeen = min(100, round((self.maxContentOffset + self.view.frame.height) / self.scrollView.contentSize.height * 100))
     let result = " Profile seen: \(percentProfileSeen)%, Scroll Distance: \(round(self.totalScrollDistance / self.scrollView.contentSize.height * 100) / 100)X in \(round((CACurrentMediaTime() - initializationTime) * 100) / 100)s"
-    print(result)
     return result
   }
   
