aae07396369dc23f82a02752dd11e44f3d246d90
Avery Lamp <averylamp@gmail.com>
2019-06-19 18:32:50 -0400

    Splits Data Store into more files

---
+++ Pear/Shared/Data Store/DataStore+Matches.swift
@@ -0,0 +1,86 @@
+//
+//  DataStore+Matches.swift
+//  Pear
+//
+//  Created by Avery Lamp on 6/19/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Data Store Matches Extensions
+extension DataStore {
+  
+  /// Refreshes the current matches that the current user has received
+  ///
+  /// - Parameter matchRequestsFound: Runs a completion when the matches have completed fetching
+  func refreshCurrentMatches(matchRequestsFound: (([Match]) -> Void)?) {
+    self.fetchUIDToken { (result) in
+      switch result {
+      case .success(let authTokens):
+        PearMatchesAPI.shared.getMatchesForUser(uid: authTokens.uid,
+                                                token: authTokens.token,
+                                                matchType: .currentMatches,
+                                                completion: { (result) in
+                                                  switch result {
+                                                  case .success(let matches):
+                                                    self.currentMatches = matches
+                                                    print("Current Matches:\(self.matchRequests.count)")
+                                                    NotificationCenter.default.post(name: .refreshChatsTab, object: nil)
+                                                    if let matchCompletion = matchRequestsFound {
+                                                      matchCompletion(matches)
+                                                    }
+                                                  case .failure(let error):
+                                                    print("Failure getting error: \(error)")
+                                                    if let matchCompletion = matchRequestsFound {
+                                                      matchCompletion([])
+                                                    }
+                                                  }
+        })
+      case .failure(let error):
+        print("Failure getting Tokens: \(error)")
+        if let matchCompletion = matchRequestsFound {
+          matchCompletion([])
+        }
+        return
+      }
+    }
+  }
+  
+  /// Refreshes the match requests a user has received
+  ///
+  /// - Parameter matchRequestsFound: optional completion for after a refresh occurs
+  func refreshMatchRequests(matchRequestsFound: (([Match]) -> Void)?) {
+    self.fetchUIDToken { (result) in
+      switch result {
+      case .success(let authTokens):
+        PearMatchesAPI.shared.getMatchesForUser(uid: authTokens.uid,
+                                                token: authTokens.token,
+                                                matchType: .matchRequests,
+                                                completion: { (result) in
+                                                  switch result {
+                                                  case .success(let matches):
+                                                    self.matchRequests = matches
+                                                    print("Match Requests:\(self.matchRequests.count)")
+                                                    NotificationCenter.default.post(name: .refreshChatsTab, object: nil)
+                                                    if let matchCompletion = matchRequestsFound {
+                                                      matchCompletion(matches)
+                                                    }
+                                                  case .failure(let error):
+                                                    print("Failure getting error: \(error)")
+                                                    if let matchCompletion = matchRequestsFound {
+                                                      matchCompletion([])
+                                                    }
+                                                  }
+        })
+      case .failure(let error):
+        print("Failure getting Tokens: \(error)")
+        if let matchCompletion = matchRequestsFound {
+          matchCompletion([])
+        }
+        return
+      }
+    }
+  }
+
+}
