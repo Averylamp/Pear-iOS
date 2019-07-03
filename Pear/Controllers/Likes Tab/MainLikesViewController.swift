fb8e9422fc9c1935d3af3da43ca37ca08d7e4db3
Avery Lamp <averylamp@gmail.com>
2019-07-03 00:41:29 -0400

    Updates tab bar and also preloads vcs

--- Pear/Controllers/Likes Tab/MainLikesViewController.swift
+++ Pear/Controllers/Likes Tab/MainLikesViewController.swift
@@ -35,6 +35,7 @@ extension MainLikesViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    print("Main Likes VC Loaded")
     self.setup()
     self.stylize()
     self.refreshRequests()
@@ -48,30 +49,7 @@ extension MainLikesViewController {
   
   /// Setup should only be called once
   func setup() {
-    guard let requestsTVC = ChatRequestsTableViewController.instantiate(tableViewType: .requests) else {
-      print("Failed to instantiate requests TVC")
-      return
-    }
-    self.requestsTVC = requestsTVC
-    requestsTVC.delegate = self
-    self.addChild(requestsTVC)
-    self.scrollView.addSubview(requestsTVC.view)
-    requestsTVC.view.translatesAutoresizingMaskIntoConstraints = false
-    self.scrollView.addConstraints([
-      NSLayoutConstraint(item: requestsTVC.view as Any, attribute: .width, relatedBy: .equal,
-                         toItem: self.scrollView, attribute: .width, multiplier: 1.0, constant: 0.0),
-      NSLayoutConstraint(item: requestsTVC.view as Any, attribute: .height, relatedBy: .equal,
-                         toItem: self.scrollView, attribute: .height, multiplier: 1.0, constant: 0.0),
-      NSLayoutConstraint(item: requestsTVC.view as Any, attribute: .centerX, relatedBy: .equal,
-                         toItem: self.scrollView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
-      NSLayoutConstraint(item: requestsTVC.view as Any, attribute: .centerY, relatedBy: .equal,
-                         toItem: self.scrollView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
-      ])
-    requestsTVC.didMove(toParent: self)
-    requestsTVC.updateMatches(matches: DataStore.shared.matchRequests)
-    self.view.layoutIfNeeded()
-    requestsTVC.tableView.refreshControl = self.requestsRefreshControl
-    self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+    
   }
   
   /// Stylize can be called more than once
@@ -80,47 +58,17 @@ extension MainLikesViewController {
   }
 
   func refreshRequests() {
-    if let requestVC = self.requestsTVC {
-      DataStore.shared.refreshMatchRequests { (matchRequests) in
-        DispatchQueue.main.async {
-          print("Updating with \(matchRequests.count) match requests")
-          requestVC.updateMatches(matches: matchRequests)
-          self.requestsRefreshControl.endRefreshing()
+    DataStore.shared.refreshMatchRequests { (matchRequests) in
+      DispatchQueue.main.async {
+        print("Updating with \(matchRequests.count) match requests")
+        if matchRequests.count > 0 {
+          self.tabBarItem.badgeValue = "\(matchRequests.count)"
+          self.tabBarItem.badgeColor = UIColor(red: 1.00, green: 0.72, blue: 0.29, alpha: 1.00)
+        } else {
+          self.tabBarItem.badgeValue = nil
         }
+        self.requestsRefreshControl.endRefreshing()
       }
     }
   }
-}
-// MARK: - ChatTVDelegate
-extension MainLikesViewController: ChatRequestTableViewControllerDelegate {
-  
-  func selectedMatch(match: Match) {
-    guard let chat = match.chat else {
-      print("Failed to get chat for match")
-      return
-    }
-    
-    DispatchQueue.main.async {
-      HapticFeedbackGenerator.generateHapticFeedbackImpact(style: .light)
-      guard let fullRequestVC = LikeFullProfileViewController.instantiate(match: match) else {
-        print("Failed to instantiate full chat VC")
-        return
-      }
-      self.navigationController?.pushViewController(fullRequestVC, animated: true)
-      
-    }
-  }
-  
-  func selectedProfile(user: PearUser) {
-    DispatchQueue.main.async {
-      HapticFeedbackGenerator.generateHapticFeedbackImpact(style: .light)
-      let fullProfile = FullProfileDisplayData(user: user)
-      guard let fullProfileScrollVC = FullProfileScrollViewController.instantiate(fullProfileData: fullProfile) else {
-        print("failed to instantiate full profile scroll VC")
-        return
-      }
-      self.navigationController?.pushViewController(fullProfileScrollVC, animated: true)
-    }
-  }
-  
 }
