377bf8a644892a9655dc5fd0e18dd4a5ef33259d
Avery Lamp <averylamp@gmail.com>
2019-07-10 18:29:24 -0400

    Loads filter items properly

--- Pear/Controllers/Discovery Flow/Discovery Filter Overlay/DiscoveryFilterItemTableViewCell.swift
+++ Pear/Controllers/Discovery Flow/Discovery Filter Overlay/DiscoveryFilterItemTableViewCell.swift
@@ -14,29 +14,30 @@ class DiscoveryFilterItemTableViewCell: UITableViewCell {
   let nameLabel = UILabel()
   let selectedImageView = UIImageView()
   static let sideOffsets: CGFloat = 12
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    self.addSubview(self.thumbnailImageView)
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.contentView.addSubview(self.thumbnailImageView)
     self.thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
-    self.addSubview(self.nameLabel)
+    self.contentView.addSubview(self.nameLabel)
     self.nameLabel.translatesAutoresizingMaskIntoConstraints = false
-    self.addSubview(self.selectedImageView)
+    self.contentView.addSubview(self.selectedImageView)
     self.selectedImageView.translatesAutoresizingMaskIntoConstraints = false
     
     // Thumbnail Image Constraints
-    self.addConstraints([
+    self.contentView.addConstraints([
       NSLayoutConstraint(item: self.thumbnailImageView, attribute: .left, relatedBy: .equal,
-                         toItem: self, attribute: .left, multiplier: 1.0, constant: DiscoveryFilterItemTableViewCell.sideOffsets),
+                         toItem: self.contentView, attribute: .left, multiplier: 1.0, constant: DiscoveryFilterItemTableViewCell.sideOffsets),
       NSLayoutConstraint(item: self.thumbnailImageView, attribute: .centerY, relatedBy: .equal,
-                         toItem: self, attribute: .centerY, multiplier: 1.0, constant: 0.0),
+                         toItem: self.contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0),
       NSLayoutConstraint(item: self.thumbnailImageView, attribute: .height, relatedBy: .equal,
-                         toItem: self, attribute: .height, multiplier: 1.0, constant: -8),
+                         toItem: self.contentView, attribute: .height, multiplier: 1.0, constant: -20.0),
       NSLayoutConstraint(item: self.thumbnailImageView, attribute: .height, relatedBy: .equal,
-                         toItem: self, attribute: .width, multiplier: 1.0, constant: 0.0)
+                         toItem: self.thumbnailImageView, attribute: .width, multiplier: 1.0, constant: 0.0)
       ])
     
     // Label Constraints
-    self.addConstraints([
+    self.contentView.addConstraints([
       NSLayoutConstraint(item: self.nameLabel, attribute: .left, relatedBy: .equal,
                          toItem: self.thumbnailImageView, attribute: .right, multiplier: 1.0, constant: 12.0),
       NSLayoutConstraint(item: self.nameLabel, attribute: .centerY, relatedBy: .equal,
@@ -44,33 +45,37 @@ class DiscoveryFilterItemTableViewCell: UITableViewCell {
       ])
     
     // Selected Image Constraints
-    self.addConstraints([
+    self.contentView.addConstraints([
       NSLayoutConstraint(item: self.selectedImageView, attribute: .right, relatedBy: .equal,
-                         toItem: self, attribute: .right, multiplier: 1.0, constant: -DiscoveryFilterItemTableViewCell.sideOffsets),
+                         toItem: self.contentView, attribute: .right, multiplier: 1.0, constant: -DiscoveryFilterItemTableViewCell.sideOffsets),
       NSLayoutConstraint(item: self.selectedImageView, attribute: .centerY, relatedBy: .equal,
                          toItem: self.thumbnailImageView, attribute: .centerY, multiplier: 1.0, constant: 0.0),
       NSLayoutConstraint(item: self.selectedImageView, attribute: .height, relatedBy: .equal,
-                         toItem: self, attribute: .height, multiplier: 1.0, constant: -8),
+                         toItem: self.contentView, attribute: .height, multiplier: 1.0, constant: -20),
       NSLayoutConstraint(item: self.selectedImageView, attribute: .height, relatedBy: .equal,
-                         toItem: self, attribute: .width, multiplier: 1.0, constant: 0.0),
+                         toItem: self.selectedImageView, attribute: .width, multiplier: 1.0, constant: 0.0),
       NSLayoutConstraint(item: self.nameLabel, attribute: .right, relatedBy: .equal,
                          toItem: self.selectedImageView, attribute: .left, multiplier: 1.0, constant: -8.0)
       ])
     
-    self.layoutIfNeeded()
     // Stylizes Thumbnail
     self.thumbnailImageView.contentMode = .scaleAspectFill
     self.thumbnailImageView.layer.cornerRadius = self.thumbnailImageView.frame.height / 2.0
     self.thumbnailImageView.clipsToBounds = true
     
     // Stylizes Label
-    if let font = R.font.openSansBold(size: 18.0){
+    if let font = R.font.openSansBold(size: 18.0) {
       self.nameLabel.font = font
     }
     
     // Stylizes Selected Image
     self.selectedImageView.contentMode = .scaleAspectFit
+    self.selectedImageView.backgroundColor = UIColor.green
     
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
@@ -84,6 +89,5 @@ class DiscoveryFilterItemTableViewCell: UITableViewCell {
     self.nameLabel.text = firstName
     self.selectedImageView.alpha = selected ? 1.0 : 0.0
   }
-  
   
 }
