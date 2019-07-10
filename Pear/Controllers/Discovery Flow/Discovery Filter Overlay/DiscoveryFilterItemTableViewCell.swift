34b6be05ff4f7558e92648f25631eef64abe1b4d
Avery Lamp <averylamp@gmail.com>
2019-07-10 17:44:57 -0400

    Animates filters in and out

---
+++ Pear/Controllers/Discovery Flow/Discovery Filter Overlay/DiscoveryFilterItemTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DiscoveryFilterItemTableViewCell.swift
+//  Pear
+//
+//  Created by Avery Lamp on 7/10/19.
+//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryFilterItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
