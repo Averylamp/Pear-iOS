d86e1af7c62014867ce3b48eef92cfc3b60f8de9
Avery Lamp <averylamp@gmail.com>
2019-07-10 15:21:40 -0400

    Standardizes animation time

--- Pear/Controllers/Discovery Flow/Discovery Full Profile View/DiscoveryFullProfileContainerViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DiscoveryFullProfileContainerViewController.swift
-//  Pear
-//
-//  Created by Avery Lamp on 7/7/19.
-//  Copyright © 2019 Setup and Matchmake Inc. All rights reserved.
-//
-
-import Foundation
